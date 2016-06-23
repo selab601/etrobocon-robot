c5a3b399edc5db37a7e1d3f9a752fdb30a1315be
motoki nagaoka <motoki.23ar@gmail.com>
2016-06-24 06:28:31 +0900

    PIDパラーメータの調整・テスト

--- contest/Contest.cpp
+++ contest/Contest.cpp
@@ -43,9 +43,21 @@ namespace contest {
         {
             if (!initialized) {
                 initialized = true;
-                drive::LineTrace::getInstance()->setPID(1.0, 1.0, 1.0);
+                // drive::LineTrace::getInstance()->setPID(1.0, 0.001, 0.000001);
+                // drive::LineTrace::getInstance()->setPID(0.0005, 0.00000001, 0.0005);
+                //drive::LineTrace::getInstance()->setPID(0.006, 0.0, 0.01);
+
+                // drive::LineTrace::getInstance()->setPID(0.006, 0.0, 0.024); //わりとよかった(20, 0.5
+                // drive::LineTrace::getInstance()->setPID(0.024, 0.0, 0.024); // 灰色走れる(20, 0.5)
+                drive::LineTrace::getInstance()->setPID(0.024, 0.0, 0.048); // 灰色走れる, ギザギザだけどRコースの変なカーブいける！(20, 0.5)
+                // drive::LineTrace::getInstance()->setPID(0.018, 0.0, 0.048); // 灰色走れる,これでもギザギザだけどRコースの変なカーブいける！(20, 0.5)
+                // drive::LineTrace::getInstance()->setPID(0.018, 0.0, 0.06); // もっとギザギザだけど普通に走れる(20, 0.5)
+
+                // drive::LineTrace::getInstance()->setPID(0.0063, 0.0, 0.06); // ブレブレだけど普通に走れる(50, 0.5)
+                // drive::LineTrace::getInstance()->setPID(0.0075, 0.0, 0.0024);
+                // drive::LineTrace::getInstance()->setPID(0.0144, 0.000072, 0.72);
+                drive::LineTrace::getInstance()->setPID(0.0144, 0.0, 0.72); // めっちゃ安定する！（30, 0.6);
             }
-            drive::LineTrace::getInstance()->run(40, 0.5);
             break;
         }
           case 'R':
@@ -59,8 +71,10 @@ namespace contest {
         if ( starter->start() ){
           switch (su->getSelectedCourse() ){
           case 'L':
+            drive::LineTrace::getInstance()->run(100, 0.6);
             break;
           case 'R':
+            drive::LineTrace::getInstance()->run(20);
             break;
           }
         }
