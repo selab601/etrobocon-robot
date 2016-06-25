a2c19406c54e094fc43c1f51ed73965620514890
tasuwo <kamuhata.you@gmail.com>
2016-06-25 20:00:37 +0900

    Display に namespace の定義が漏れていたので追加

--- device/Display.h
+++ device/Display.h
@@ -7,57 +7,60 @@
 #include <string>
 #include <stdlib.h>
 
-class Display
+namespace device
 {
-private:
-    //! 共有インスタンス
-    static Display* instance_;
+    class Display
+    {
+    private:
+        //! 共有インスタンス
+        static Display* instance_;
 
-    // フォントサイズ
-    int32_t font_width_;
-    int32_t font_height_;
+        // フォントサイズ
+        int32_t font_width_;
+        int32_t font_height_;
 
-    //! 出力可能文字数
-    uint8_t max_output_chars_;    //!< 出力可能文字数
-    //! 出力文字列のバッファ
-    char* output_str_buffer_;
+        //! 出力可能文字数
+        uint8_t max_output_chars_;    //!< 出力可能文字数
+        //! 出力文字列のバッファ
+        char* output_str_buffer_;
 
-private:
-    Display();
+    private:
+        Display();
 
-public:
-    static Display* getInstance();
+    public:
+        static Display* getInstance();
 
-    /**
-     * @brief 出力フォントを設定する
-     * @param font_type 出力フォント(EV3_FONT_SMALL, EV3_FONT_MIDIUM)
-     */
-    void setFont(lcdfont_t font_type);
+        /**
+         * @brief 出力フォントを設定する
+         * @param font_type 出力フォント(EV3_FONT_SMALL, EV3_FONT_MIDIUM)
+         */
+        void setFont(lcdfont_t font_type);
 
-    /**
-     * @brief LCDをクリアーする
-     * @details LCD画面を白で塗りつぶす
-     */
-    void clearDisplay();
+        /**
+         * @brief LCDをクリアーする
+         * @details LCD画面を白で塗りつぶす
+         */
+        void clearDisplay();
 
-    /**
-     * @brief 文字列の出力
-     * @details 引数の文字列を指定行に出力する
-     *
-     * @param str 出力文字列
-     * @param col 出力行
-     */
-    void updateDisplay(const char* str, int col);
+        /**
+         * @brief 文字列の出力
+         * @details 引数の文字列を指定行に出力する
+         *
+         * @param str 出力文字列
+         * @param col 出力行
+         */
+        void updateDisplay(const char* str, int col);
 
-    /**
-     * @brief 文字列及び数値の出力
-     * @details 「文字列：数値」の形式で，指定行に出力する
-     *
-     * @param str 出力文字列
-     * @param num 出力数値
-     * @param col 出力行
-     */
-    void updateDisplay(const char* str, int num, int col);
-};
+        /**
+         * @brief 文字列及び数値の出力
+         * @details 「文字列：数値」の形式で，指定行に出力する
+         *
+         * @param str 出力文字列
+         * @param num 出力数値
+         * @param col 出力行
+         */
+        void updateDisplay(const char* str, int num, int col);
+    };
+}
 
 #endif
