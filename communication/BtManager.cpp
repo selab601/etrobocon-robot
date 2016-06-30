95ad96e938f4b98d0fc5c59384ef2a6eec14e0ea
motoki nagaoka <16nm719r@vc.ibaraki.ac.jp>
2016-06-30 11:35:26 +0900

    インデントをスペースに

--- communication/BtManager.cpp
+++ communication/BtManager.cpp
@@ -2,135 +2,135 @@
 
 namespace communication {
 
-	/* メンバ */
-	BtManager* BtManager::instance = 0;
-	// FILE* BtManager::bt;
-	// char BtManager::message[] = {0};
+    /* メンバ */
+    BtManager* BtManager::instance = 0;
+    // FILE* BtManager::bt;
+    // char BtManager::message[] = {0};
 
-	/* コンストラクタ */
-	BtManager::BtManager() {
-  clock = new Clock();
-		color = Color::getInstance();
-		gyro  = Gyro::getInstance();
-		touch = Touch::getInstance();
-		motor = TrikeMotor::getInstance();
-		loc = Localization::getInstance();
-		bt = NULL;
-		state = BT_WATING;
-	}
+    /* コンストラクタ */
+    BtManager::BtManager() {
+        clock = new Clock();
+        color = Color::getInstance();
+        gyro  = Gyro::getInstance();
+        touch = Touch::getInstance();
+        motor = TrikeMotor::getInstance();
+        loc = Localization::getInstance();
+        bt = NULL;
+        state = BT_WATING;
+    }
 
-	/* インスタンス取得 */
-	BtManager* BtManager::getInstance() {
-		if (instance == 0) {
-			instance = new BtManager();
-		}
-		return instance;
-	}
+    /* インスタンス取得 */
+    BtManager* BtManager::getInstance() {
+        if (instance == 0) {
+            instance = new BtManager();
+        }
+        return instance;
+    }
 
-	/* Bluetooth接続 */
-	bool BtManager::connect() {
-		/* つないでなかったら接続 */
-		if (!ev3_bluetooth_is_connected()) {
-			/* 音 */
-	    ev3_speaker_set_volume(1);
-	    ev3_speaker_play_tone(500, 100);
+    /* Bluetooth接続 */
+    bool BtManager::connect() {
+        /* つないでなかったら接続 */
+        if (!ev3_bluetooth_is_connected()) {
+            /* 音 */
+            ev3_speaker_set_volume(1);
+            ev3_speaker_play_tone(500, 100);
 
-	    /* 画面 */
-			ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
-    	ev3_lcd_draw_string("connecting ... ", 0, 0);
-    	ev3_lcd_draw_string("Push enter to exit", 0, 10);
+            /* 画面 */
+            ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
+            ev3_lcd_draw_string("connecting ... ", 0, 0);
+            ev3_lcd_draw_string("Push enter to exit", 0, 10);
 
-	    /* 接続待機 */
-    	int i = 0;
-	    while(!ev3_bluetooth_is_connected()) {
-	    	if (ev3_button_is_pressed(ENTER_BUTTON)) {
-	    		i++;
-	    	} else if (i > 0) {
-	    		close();
-	    		return false;
-	    	}
-	    }
+            /* 接続待機 */
+            int i = 0;
+            while(!ev3_bluetooth_is_connected()) {
+                if (ev3_button_is_pressed(ENTER_BUTTON)) {
+                    i++;
+                } else if (i > 0) {
+                    close();
+                    return false;
+                }
+            }
 
-			/* 音 */
-	    ev3_speaker_play_tone(500, 100);
+            /* 音 */
+            ev3_speaker_play_tone(500, 100);
 
-	  	/* 画面 */
-	  	ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
-	  	ev3_lcd_draw_string("connected !", 0, 0);
+            /* 画面 */
+            ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
+            ev3_lcd_draw_string("connected !", 0, 0);
 
-	    /* 接続先ポート(を、ファイルとして開く) */
-	    bt = ev3_serial_open_file(EV3_SERIAL_BT);
+            /* 接続先ポート(を、ファイルとして開く) */
+            bt = ev3_serial_open_file(EV3_SERIAL_BT);
 
-	    /* 接続開始送信 */
-     //char msg[10] = {0};
-      //sprintf(msg, "start\r\n");
-      //setMessage(msg);
+            /* 接続開始送信 */
+            //char msg[10] = {0};
+            //sprintf(msg, "start\r\n");
+            //setMessage(msg);
 
-      state = BT_CONNECTED;
-	  }
-	  return true;
-	}
+            state = BT_CONNECTED;
+        }
+        return true;
+    }
 
-	/* Bluetooth接続確認 */
-	bool BtManager::isConnected() {
-		return ev3_bluetooth_is_connected();
-	}
+    /* Bluetooth接続確認 */
+    bool BtManager::isConnected() {
+        return ev3_bluetooth_is_connected();
+    }
 
- /* Bluetooth切断 */
-	void BtManager::close() {
-		state = BT_CLOSED;
-		if (bt != NULL) {
-			fclose(bt);
-		}
-	}
+    /* Bluetooth切断 */
+    void BtManager::close() {
+        state = BT_CLOSED;
+        if (bt != NULL) {
+            fclose(bt);
+        }
+    }
 
 
-	/* 走行体情報送信 */
-	void BtManager::send() {
-		/* メッセージがなかったら情報取得 */
-		if (message[0] == '\0') {
-      /*
-			sprintf(message, "bligh:%.3d, touch:%.1d, gyro :% .3d, sonar:%.3d, dist :%.8ld\r\n",
-			        color->getBrightness(),
-			        touch->isPressed() ? 1 : 0,
-			        gyro->getGyroValue(),
-			        sonar->getSonarValue(),
-			        loc->get_migrationLength());
-      */
-      sprintf(message, "%ld,%d,%d,%d,%d,%ld,%ld,%ld,%d,%d,%d,%ld,%ld,%d\n",
-              clock->now(),
-              color->getBrightness(),
-              gyro->getGyroValue(),
-              0,
-              touch->isPressed() ? 1 : 0,
-              motor->getFrontCount(),
-              motor->getLeftCount(),
-              motor->getRightCount(),
-              0,
-              0,
-              0,
-              motor->getFrontAngle(),
-              loc->get_migrationLength(),
-              loc->getAngle());
-		}
+    /* 走行体情報送信 */
+    void BtManager::send() {
+        /* メッセージがなかったら情報取得 */
+        if (message[0] == '\0') {
+            /*
+               sprintf(message, "bligh:%.3d, touch:%.1d, gyro :% .3d, sonar:%.3d, dist :%.8ld\r\n",
+               color->getBrightness(),
+               touch->isPressed() ? 1 : 0,
+               gyro->getGyroValue(),
+               sonar->getSonarValue(),
+               loc->get_migrationLength());
+             */
+            sprintf(message, "%ld,%d,%d,%d,%d,%ld,%ld,%ld,%d,%d,%d,%ld,%ld,%d\n",
+                    clock->now(),
+                    color->getBrightness(),
+                    gyro->getGyroValue(),
+                    0,
+                    touch->isPressed() ? 1 : 0,
+                    motor->getFrontCount(),
+                    motor->getLeftCount(),
+                    motor->getRightCount(),
+                    0,
+                    0,
+                    0,
+                    motor->getFrontAngle(),
+                    loc->get_migrationLength(),
+                    loc->getAngle());
+        }
 
-		/* 送信 */
-		int size = sizeof(message);
-		int result;
-		if (size > 0) {
-		  result = fprintf(bt, message);
-		}
+        /* 送信 */
+        int size = sizeof(message);
+        int result;
+        if (size > 0) {
+            result = fprintf(bt, message);
+        }
 
-		/* 送信失敗したら通信終了 */
-		if (result < 0) {
-			close();
-		}
+        /* 送信失敗したら通信終了 */
+        if (result < 0) {
+            close();
+        }
 
-		/* メッセージ初期化 */
-		for (int i = 0; i < MESSAGE_LEN; i++) {
-			message[i] = '\0';
-		}
-	}
+        /* メッセージ初期化 */
+        for (int i = 0; i < MESSAGE_LEN; i++) {
+            message[i] = '\0';
+        }
+    }
 
     /* メッセージ受信 */
     void BtManager::receive() {
@@ -151,15 +151,15 @@ namespace communication {
         //}
     }
 
-    
-    
-	/* 指定メッセージ送信 */
-	void BtManager::setMessage(const char msg[]) {
-		sprintf(message, "%s", msg);
-	}
 
-	/* ステータス更新 */
-	BtManager::BT_STATE BtManager::getState() {
-		return state;
-	}
+
+    /* 指定メッセージ送信 */
+    void BtManager::setMessage(const char msg[]) {
+        sprintf(message, "%s", msg);
+    }
+
+    /* ステータス更新 */
+    BtManager::BT_STATE BtManager::getState() {
+        return state;
+    }
 }
--- communication/BtManager.h
+++ communication/BtManager.h
@@ -18,126 +18,126 @@
 namespace communication
 {
 
-	/**
-	 * @brief Bluetooth通信用クラスです。
-	 * connect関数で接続されるまで待機します。
-	 * send関数でデータを送信します。
-	 */
-	class BtManager
-	{
-		public:
-			/* 接続状態の列挙型 */
-			enum BT_STATE
-			{
-				BT_CONNECTED,
-				BT_CLOSED,
-				BT_WATING,
-			};
+    /**
+     * @brief Bluetooth通信用クラスです。
+     * connect関数で接続されるまで待機します。
+     * send関数でデータを送信します。
+     */
+    class BtManager
+    {
+        public:
+            /* 接続状態の列挙型 */
+            enum BT_STATE
+            {
+                BT_CONNECTED,
+                BT_CLOSED,
+                BT_WATING,
+            };
 
-		private:
-			/* このクラスのインスタンス */
-			static BtManager* instance;
+        private:
+            /* このクラスのインスタンス */
+            static BtManager* instance;
 
-			/* 通信先 */
-			FILE* bt;
+            /* 通信先 */
+            FILE* bt;
 
-   /* 時間情報 */
-   Clock* clock;
-   
-			/* 光情報 */
-			Color* color;
-			
-   /* ジャイルセンサ情報 */
-   Gyro* gyro;
+            /* 時間情報 */
+            Clock* clock;
 
-   /* ソナーセンサ情報 */
-			Sonar* sonar;
+            /* 光情報 */
+            Color* color;
 
-   /* タッチセンサ情報 */
-   Touch* touch;
+            /* ジャイルセンサ情報 */
+            Gyro* gyro;
 
-   /* モータ情報 */
-   TrikeMotor* motor;
-   
-			/* 走行情報 */
-			Localization* loc;
+            /* ソナーセンサ情報 */
+            Sonar* sonar;
 
-			/* 送信メッセージ */
-			char message[MESSAGE_LEN];
+            /* タッチセンサ情報 */
+            Touch* touch;
 
-   /* 受信メッセージ */
-   char r_message[MESSAGE_LEN];
-   
-			/* 接続状態 */
-			BT_STATE state;
+            /* モータ情報 */
+            TrikeMotor* motor;
 
-		public:
-			/**
-			 * @brief インスタンス取得
-			 * @return Bluetooth管理用インスタンス
-			 */
-			static BtManager* getInstance();
+            /* 走行情報 */
+            Localization* loc;
 
-			/**
-			 * @brief Bluetooth切断
-			 */
-			void close();
+            /* 送信メッセージ */
+            char message[MESSAGE_LEN];
 
-			/**
-			 * @brief Bluetooth接続
-			 * 接続されるまで待機します
-			 * @return 成功したらtrue
-			 */
-			bool connect();
+            /* 受信メッセージ */
+            char r_message[MESSAGE_LEN];
 
-			/**
-			 * @brief 接続状態を返す
-			 * BT_WATING : 接続待機
-			 * BT_CONNECTED : 接続中 データ送信出来ます
-			 * BT_CLOSED : 通信終了済み 以降データ送信を行いません
-			 */
-			BT_STATE getState();
+            /* 接続状態 */
+            BT_STATE state;
 
-			/**
-			 * @brief Bluetooth接続確認
-			 * @return 接続中ならtrue
-			 */
-			bool isConnected();
+        public:
+            /**
+             * @brief インスタンス取得
+             * @return Bluetooth管理用インスタンス
+             */
+            static BtManager* getInstance();
 
-			/**
-			 * @brief メッセージ送信
-			 * 走行体情報を送ります
-			 * setMessageによってメッセージがセットされている場合はそのメッセージを、
-			 * されていない場合はデフォルトのメッセージを送信します
-			 */
-			void send();
+            /**
+             * @brief Bluetooth切断
+             */
+            void close();
 
-   /**
-    * @brief メッセージ受信
-    * メッセージを受信します
-    */
-   void receive();
+            /**
+             * @brief Bluetooth接続
+             * 接続されるまで待機します
+             * @return 成功したらtrue
+             */
+            bool connect();
 
-			/**
-			 * @brief メッセージ送信
-			 * 改行またはnullまでを送信
-			 */
-			// void send(const char[]);
+            /**
+             * @brief 接続状態を返す
+             * BT_WATING : 接続待機
+             * BT_CONNECTED : 接続中 データ送信出来ます
+             * BT_CLOSED : 通信終了済み 以降データ送信を行いません
+             */
+            BT_STATE getState();
 
-			/**
-			 * @brief 送信データのセット
-			 * 送信メッセージをセットします
-			 */
-			void setMessage(const char[]);
+            /**
+             * @brief Bluetooth接続確認
+             * @return 接続中ならtrue
+             */
+            bool isConnected();
 
-		private:
-			/**
-			 * @brief コンストラクタ
-			 * 外部から呼び出せません。
-			 */
-			BtManager();
+            /**
+             * @brief メッセージ送信
+             * 走行体情報を送ります
+             * setMessageによってメッセージがセットされている場合はそのメッセージを、
+             * されていない場合はデフォルトのメッセージを送信します
+             */
+            void send();
+
+            /**
+             * @brief メッセージ受信
+             * メッセージを受信します
+             */
+            void receive();
+
+            /**
+             * @brief メッセージ送信
+             * 改行またはnullまでを送信
+             */
+            // void send(const char[]);
+
+            /**
+             * @brief 送信データのセット
+             * 送信メッセージをセットします
+             */
+            void setMessage(const char[]);
+
+        private:
+            /**
+             * @brief コンストラクタ
+             * 外部から呼び出せません。
+             */
+            BtManager();
 
 
-	};
+    };
 }
 #endif
