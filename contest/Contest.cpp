ce0d69736c9ea8acdf3482c9fb8f706c7dae9c3c
Usui Kakeru <12t4015h@Usui-no-MacBook-Pro.local>
2016-07-01 18:31:48 +0900

    linetraceブランチからLineTraceを削除

--- contest/Contest.cpp
+++ contest/Contest.cpp
@@ -1,6 +1,5 @@
 #include "Contest.h"
 
-#include "../drive/LineTrace.h"
 
 namespace contest {
 
@@ -43,27 +42,11 @@ namespace contest {
         {
             if (!initialized) {
                 initialized = true;
-                // drive::LineTrace::getInstance()->setPID(1.0, 0.001, 0.000001);
-                // drive::LineTrace::getInstance()->setPID(0.0005, 0.00000001, 0.0005);
-                //drive::LineTrace::getInstance()->setPID(0.006, 0.0, 0.01);
-
-                // drive::LineTrace::getInstance()->setPID(0.006, 0.0, 0.024); //わりとよかった(20, 0.5
-                // drive::LineTrace::getInstance()->setPID(0.024, 0.0, 0.024); // 灰色走れる(20, 0.5)
-                // drive::LineTrace::getInstance()->setPID(0.024, 0.0, 0.048); // 灰色走れる, ギザギザだけどRコースの変なカーブいける！(20, 0.5)
-                // drive::LineTrace::getInstance()->setPID(0.018, 0.0, 0.048); // 灰色走れる,これでもギザギザだけどRコースの変なカーブいける！(20, 0.5)
-                // drive::LineTrace::getInstance()->setPID(0.018, 0.0, 0.06); // もっとギザギザだけど普通に走れる(20, 0.5)
-
-                // drive::LineTrace::getInstance()->setPID(0.0063, 0.0, 0.06); // ブレブレだけど普通に走れる(50, 0.5)
-                // drive::LineTrace::getInstance()->setPID(0.0075, 0.0, 0.0024);
-                // drive::LineTrace::getInstance()->setPID(0.0144, 0.000072, 0.72);
-                // drive::LineTrace::getInstance()->setPID(0.0144, 0.0, 0.72); // めっちゃ安定する！（30, 0.6);
-                drive::LineTrace::getInstance()->setPID(0.0216, 0.0, 1.431);
             }
             break;
         }
           case 'R':
               if (!initialized) {
-                  drive::LineTrace::getInstance()->setPID();
                   initialized = true;
               }
           break;
@@ -73,10 +56,8 @@ namespace contest {
         if ( starter->start() ){
           switch (su->getSelectedCourse() ){
           case 'L':
-            drive::LineTrace::getInstance()->run(100, 0.6);
             break;
           case 'R':
-            drive::LineTrace::getInstance()->run(100);
             break;
           }
         }
