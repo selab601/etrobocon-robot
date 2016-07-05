d5e99094df90272ec7e786967eacde0031639f17
Usui Kakeru <kakeru3.141592@gmail.com>
2016-07-05 10:43:26 +0900

    タブをスペースに変更

--- contest/Section.cpp
+++ contest/Section.cpp
@@ -12,56 +12,56 @@ using namespace measurement;
 namespace contest_pkg{
 
 
-	/* コンストラクタ */
-	Section::Section(int sectionDistance, strategy::IStrategy* strategy, bool useRelativeDistance){
-		this->sectionDistance = sectionDistance;
-		this->strategy = strategy;
+    /* コンストラクタ */
+    Section::Section(int sectionDistance, strategy::IStrategy* strategy, bool useRelativeDistance){
+        this->sectionDistance = sectionDistance;
+        this->strategy = strategy;
 
-		selfPositionEstimation = SelfPositionEstimation::getInstance();
+        selfPositionEstimation = SelfPositionEstimation::getInstance();
 
-		isCaptured = false;
-		isChecked = false;
-		isStarted = false;
-		this->useRelativeDistance = useRelativeDistance;
-		startDistance = 123;
-	}
+        isCaptured = false;
+        isChecked = false;
+        isStarted = false;
+        this->useRelativeDistance = useRelativeDistance;
+        startDistance = 123;
+    }
 
-	/* 区間攻略 */
-	bool Section::capture(){
-		char message[30];
-		sprintf (message, "rel:%d, start:%d, dst:%d", useRelativeDistance, startDistance, sectionDistance);
-		ev3_lcd_draw_string(message, 0, 0);
-		// 区間の始まりの位置を記録する
-		if ( ! isStarted ){
-			startDistance = selfPositionEstimation->getMigrationLength();
-			isStarted = true;
-		}
-		//戦略を攻略する
-		if ( strategy->capture() ){
-			isCaptured = true;
-		}
+    /* 区間攻略 */
+    bool Section::capture(){
+        char message[30];
+        sprintf (message, "rel:%d, start:%d, dst:%d", useRelativeDistance, startDistance, sectionDistance);
+        ev3_lcd_draw_string(message, 0, 0);
+        // 区間の始まりの位置を記録する
+        if ( ! isStarted ){
+            startDistance = selfPositionEstimation->getMigrationLength();
+            isStarted = true;
+        }
+        //戦略を攻略する
+        if ( strategy->capture() ){
+            isCaptured = true;
+        }
 
-		if ( checkDistance() ){
-			isChecked = true;
-		}
+        if ( checkDistance() ){
+        isChecked = true;
+        }
 
-		if ( isCaptured && isChecked == true ){
-			return true;
-		}
+        if ( isCaptured && isChecked == true ){
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/* 走行距離確認 */
-	bool Section::checkDistance(){
-		// 絶対距離を使うとき
-		if ( !useRelativeDistance ){
-			return  selfPositionEstimation->getMigrationLength() > sectionDistance;
-		}
+    /* 走行距離確認 */
+    bool Section::checkDistance(){
+        // 絶対距離を使うとき
+        if ( !useRelativeDistance ){
+            return  selfPositionEstimation->getMigrationLength() > sectionDistance;
+        }
 
-		// 相対距離を使うとき
-		else {
-			return (selfPositionEstimation->getMigrationLength() - startDistance) > sectionDistance;
-		}
-	}
+        // 相対距離を使うとき
+        else {
+            return (selfPositionEstimation->getMigrationLength() - startDistance) > sectionDistance;
+        }
+    }
 }
--- contest/Section.h
+++ contest/Section.h
@@ -14,56 +14,56 @@
 
 namespace contest_pkg{
 
-	class Section{
-		private:
-			//区間距離
-			int sectionDistance;
+    class Section{
+        private:
+            //区間距離
+            int sectionDistance;
 
-			//戦略完了
-			bool isCaptured;
+            //戦略完了
+            bool isCaptured;
 
-			//走行距離確認完了
-			bool isChecked;
+            //走行距離確認完了
+            bool isChecked;
 
-			// 相対距離を使って区間を切り替えるかどうか
-			bool useRelativeDistance;
+            // 相対距離を使って区間を切り替えるかどうか
+            bool useRelativeDistance;
 
-			// スタートした位置
-			int startDistance;
+            // スタートした位置
+            int startDistance;
 
-			// スタートしたかどうか
-			int isStarted;
+            // スタートしたかどうか
+            int isStarted;
 
-			//戦略：戦略
-			strategy::IStrategy* strategy;
+            //戦略：戦略
+            strategy::IStrategy* strategy;
 
-			//自己位置推定
-			measurement::SelfPositionEstimation* selfPositionEstimation;
+            //自己位置推定
+            measurement::SelfPositionEstimation* selfPositionEstimation;
 
-		public:
-			/**
-			 * @brief コンストラクタ
-			 * @details 区間距離と戦略を割り当てる
-			 *
-			 * @param sectionDistance 区間距離
-			 * @param strategy 戦略
-			 * @param useRelativeDistance 相対距離を使って区間を切り替えるとき:true
-			 */
-			Section(int sectionDistance,strategy::IStrategy* strategy, bool useRelativeDistance = false);
-			/**
-			 * @brief 区間を攻略する
-			 * @details 戦略クラスの攻略するを呼び出す
-			 * @return 攻略済み:true,攻略中:false
-			 */
-			bool capture();
+        public:
+            /**
+             * @brief コンストラクタ
+             * @details 区間距離と戦略を割り当てる
+             *
+             * @param sectionDistance 区間距離
+             * @param strategy 戦略
+             * @param useRelativeDistance 相対距離を使って区間を切り替えるとき:true
+             */
+            Section(int sectionDistance,strategy::IStrategy* strategy, bool useRelativeDistance = false);
+            /**
+             * @brief 区間を攻略する
+             * @details 戦略クラスの攻略するを呼び出す
+             * @return 攻略済み:true,攻略中:false
+             */
+            bool capture();
 
-			/**
-			 * @brief 走行距離を確認する
-			 * @details 区間距離と自己位置の差を計算する
-			 * @return 自己位置が区間距離を越えている:true,越えていない:false
-			 */
-			bool checkDistance();
+            /**
+             * @brief 走行距離を確認する
+             * @details 区間距離と自己位置の差を計算する
+             * @return 自己位置が区間距離を越えている:true,越えていない:false
+             */
+            bool checkDistance();
 
-	};
+    };
 }
 #endif
