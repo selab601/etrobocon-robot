39d372880c4bb586e82835fcb1d7903dcf4a9217
Usui Kakeru <kakeru3.141592@gmail.com>
2016-07-06 18:46:10 +0900

    一時停止の処理を追加、デバッグ用のdisplayを削除

--- drive/ExtrusionRunning.cpp
+++ drive/ExtrusionRunning.cpp
@@ -8,26 +8,41 @@ namespace drive{
     ExtrusionRunning::ExtrusionRunning(){
         straightRunning_ = StraightRunning::getInstance();
         distanceMeasurement_ = new DistanceMeasurement();
+        timeMeasurement_ = new TimeMeasurement();
         runningState_ = INIT;
     }
 
 
     bool ExtrusionRunning::run(int speed , int distance){
         switch(runningState_){
+        //初期化...目標距離のセットをする
         case INIT:
             distanceMeasurement_->setTargetDistance(distance);
             distanceMeasurement_->startMeasurement();
             runningState_ = FORWARD;
             break;
 
+        //前進
         case FORWARD:
             if(specifiedDistanceRunning(speed)){
+                timeMeasurement_->setBaseTime();
+                timeMeasurement_->setTargetTime(500);
+                runningState_ = STOP;
+            }
+            break;
+
+        //一時停止...いきなりバックすると車体がぶれるため
+        case STOP:
+            straightRunning_->run(0);
+            if(timeMeasurement_->getResult()){
+                distanceMeasurement_->setTargetDistance(distance);
                 distanceMeasurement_->startMeasurement();
                 runningState_ = BACKWARD;
             }
-            break;
+
+        //後退
         case BACKWARD:
-            if(specifiedDistanceRunning(speed)){
+            if(specifiedDistanceRunning(-speed)){
                 return true;
             }
             break;
@@ -35,6 +50,7 @@ namespace drive{
         return false;
     }
 
+    //指定距離走行
     bool ExtrusionRunning::specifiedDistanceRunning(int speed){
         straightRunning_->run(speed);
         if(distanceMeasurement_->getResult()){
--- drive/ExtrusionRunning.h
+++ drive/ExtrusionRunning.h
@@ -3,6 +3,7 @@
 
 #include "straightRunning.h"
 #include "../measurement/DistanceMeasurement.h"
+#include "../measurement/TimeMeasurement.h"
 
 namespace drive{
     class ExtrusionRunning{
@@ -14,17 +15,24 @@ namespace drive{
             //距離検知
             measurement::DistanceMeasurement* distanceMeasurement_;
 
+            //時間検知
+            measurement::TimeMeasurement* timeMeasurement_;
+
             //走行状態
             enum RunningState
             {
                 INIT,       //初期化
                 FORWARD,    //前進
+                STOP,       //一時停止
                 BACKWARD    //後退
             };
 
             RunningState runningState_;
 
         public:
+            //コンストラクタ
+            ExtrusionRunning();
+
             /**
              * @brief 押し出し走行を行う
              * @param speed (0 < speed ≦ 100) スピード
@@ -32,9 +40,6 @@ namespace drive{
              * @return 走行終了:true,走行中:false
              */
             bool run(int speed,int distance);
-
-            //コンストラクタ
-            ExtrusionRunning();
 
         private:
             /**
