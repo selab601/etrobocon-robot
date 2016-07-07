bfb58f525e5a141bdbd033b2a40d7c2620a84155
tsurumi <15nm721f@vc.ibaraki.ac.jp>
2016-07-07 12:14:38 +0900

    シングルトンで書かれていたコードの修正，ファイル名の修正

--- drive/StraightRunning.h
+++ drive/StraightRunning.h
@@ -1,3 +1,7 @@
+/*
+ * @brief 直進走行クラス
+ */
+
 #ifndef STRAIGHT_RUNNING_H_
 #define STRAIGHT_RUNNING_H_
 
@@ -11,22 +15,15 @@ namespace drive{
 		private:
 			device::Motors* motor_;
 
-			//インスタンス
-			static StraightRunning* instance;
+		public:			
+			//コンストラクタ
+			StraightRunning();
 			
-		public:
-			//インスタンス取得
-			static StraightRunning* getInstance();
-
 			/**
 			 * @brief 直進する．
 		 	 * @param speed (-100~100)．	正の値で前進，負の値で後退
 			 */
 			void run(int speed);
-
-		private:
-			//コンストラクタ
-			StraightRunning();
 	};
 };
 #endif
