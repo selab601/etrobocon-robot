8537468793032669c43ddba4037d1ad1f07693bf
johta sisido <johta.sisido@gmail.com>
2016-07-11 15:00:34 +0900

    コメントを詳細化．ソースコードを見やすくした

--- detection/LineDetection.h
+++ detection/LineDetection.h
@@ -13,7 +13,7 @@
 namespace detection {
     /**
      * @brief ライン検知クラス
-     * @details 進んだ距離あたりのカラーセンサの値の変化量からライン検知を行う
+     * @details 進んだ距離あたりのカラーセンサの値の輝度変化量からライン検知を行う
      */
     class LineDetection {
         private:
@@ -37,6 +37,9 @@ namespace detection {
              * @brief ラインを検知する
              * @details 内部状態更新もこのメソッドで同時に行っているので、検知したい場所の前から何回も呼び出していなければならない
              * @param changeRate ライン検知の精度を変える変数．与えられなければデフォルト値を与えて実行．
+             *        changeRateを低くすると検知の精度が高く（甘く）なる．低すぎると影の変化とか多少の変化でも検知してしまうかもしれない．
+             *        MAX:白のキャリブレーション値から黒のキャリブレーション値を引いた値（例:74-6=68)
+             *        min:0
              * @return ラインを検知したとき: true, 検知していないとき: false
              */
             bool getResult(float changeRate = CHANGE_RATE);
