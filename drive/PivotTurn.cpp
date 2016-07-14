57ac95b634d93613e6b2c1e71224ec54df5c009c
tsurumi <15nm721f@vc.ibaraki.ac.jp>
2016-07-14 14:58:32 +0900

    レビュー事項の反映

--- drive/PivotTurn.cpp
+++ drive/PivotTurn.cpp
@@ -5,46 +5,40 @@
 #include "PivotTurn.h"
 
 namespace drive{
-    PivotTurn::PivotTurn(){
-        motor_ = device::Motors::getInstance();
-        bodyAngleMeasurement_ = measurement::BodyAngleMeasurement();
-        state_ = prepare_;
-    }
-
-  bool PivotTurn::turn(int degree_){
-    return turn(degree_, DEFAULT_TURN_SPEED);
+  PivotTurn::PivotTurn(){
+    motor_ = device::Motors::getInstance();
+    bodyAngleMeasurement_ = measurement::BodyAngleMeasurement();
+    phase_ = Phase::STANDBY;
   }
 
   bool PivotTurn::turn(int degree_, int speed_){
-        switch(state_){
+    switch(phase_){
+      //準備
+      case Phase::STANDBY:
+        bodyAngleMeasurement_.setBaceAngle();
+        phase_ = Phase::IN_PROGRESS;
+        break;
 
-        //準備
-        case prepare_:
-          bodyAngleMeasurement_.setBaceAngle();
-          state_ = isTurning_;
-          break;
-
-        //旋回
-        case isTurning_:
-            //目標角度まで旋回
-            if(bodyAngleMeasurement_.getResult() == degree_ || bodyAngleMeasurement_.getResult() == -degree_){
-              state_ = isTurned_;
-              motor_->setWheelPWM(0, 0);
-            }else{
-              if(degree_ < 0){speed_ *= -1;}
-              motor_->setWheelPWM(-speed_,speed_);
-            }
-            break;
-        
-        //停止
-        case isTurned_:
+      //旋回
+      case Phase::IN_PROGRESS:
+        //絶対値を取り，旋回角度が目標角度を越えたら
+        if(abs(bodyAngleMeasurement_.getResult()) >= abs(degree_)){
+          phase_ = Phase::DONE;
           motor_->setWheelPWM(0, 0);
-          //初期値にする
-          state_ = prepare_;
-          return true;
-          break;
+        }else{
+          if(degree_ < 0){speed_ *= -1;}
+          motor_->setWheelPWM(-speed_,speed_);
         }
-
-        return false;
+        break;
+        
+      //停止
+      case Phase::DONE:
+        motor_->setWheelPWM(0, 0);
+        //初期値にする
+        phase_ = Phase::STANDBY;
+        return true;
+        break;
+    }
+    return false;
   }
 }
--- drive/PivotTurn.h
+++ drive/PivotTurn.h
@@ -7,45 +7,40 @@
 
 #include "../device/Motors.h"
 #include "../measurement/BodyAngleMeasurement.h"
+#include "cstdlib" //abs() for integer
 
 #define DEFAULT_TURN_SPEED 20
 
 namespace drive
 {
-    class PivotTurn{
+  class PivotTurn{
     public:
-        /**
-         * @brief コンストラクタ
-         * @details [long description]
-         */
-        PivotTurn();
+      /**
+       * @brief コンストラクタ
+       * @details [long description]
+       */
+      PivotTurn();
 
-        /**
-         * @brief degree度回転させる 左が正，右が負
-         * @details 左方向に90度回転させるとき:turn(90)
-         * @param degree 回転させる角度
-         * @return 終了した場合true
-         */
-      bool turn(int degree_);
-
-        /**
-         * @param degree 回転させる角度
-         * @param speed  回転させる速さ(0~100, デフォルト:20)
-         * @return 終了した場合true
-         */
-      bool turn(int degree_, int speed_);
+      /**
+       * @brief degree度回転させる 左が正，右が負
+       * @details 左方向に90度回転させるとき:turn(90)
+       * @param degree 回転させる角度
+       * @param speed  回転させる速さ(0~100, デフォルト:20)
+       * @return 終了した場合true
+       */
+      bool turn(int degree_, int speed_ = DEFAULT_TURN_SPEED);
 
     private:
-        device::Motors* motor_;
-        measurement::BodyAngleMeasurement bodyAngleMeasurement_;
+      device::Motors* motor_;
+      measurement::BodyAngleMeasurement bodyAngleMeasurement_;
         
-        enum State
-        {
-            prepare_,
-            isTurning_,
-            isTurned_
-        };
-        State state_;
-    };
+      enum class Phase
+      {
+        STANDBY,
+        IN_PROGRESS,
+        DONE
+      };
+      Phase phase_;
+  };
 }
 #endif
