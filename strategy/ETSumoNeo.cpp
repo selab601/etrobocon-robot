03ef4b1596e3620fd3b66a3eba3c62d277dbae8e
Usui Kakeru <kakeru3.141592@gmail.com>
2016-07-19 13:33:51 +0900

    星取取得から登壇まで動くもの

--- strategy/ETSumoNeo.cpp
+++ strategy/ETSumoNeo.cpp
@@ -2,18 +2,104 @@
 
 using namespace drive;
 using namespace detection;
+using namespace measurement;
+
 namespace strategy{
-    ETSumoNeo::ETSumoNeo(int a){
+
+    ETSumoNeo::ETSumoNeo(){
         linetrace_ = LineTrace::getInstance();
         straightRunning_ = new StraightRunning();
         climbingRunning_ = new ClimbingRunning();
         extrusionRunning_ = new ExtrusionRunning();
+        pivotTurn_ = new PivotTurn();
         lineDetection_ = new LineDetection();
-        a_ = a;
+        colorDetection_ = new ColorDetection();
+        objectDetection_ = new ObjectDetection();
+        distanceMeasurement_ = new DistanceMeasurement();
+        statas_ = Statas::HOSHITORI;
+        //sumoStatas = SmoStatas:: ;
     }
 
     bool ETSumoNeo::capture(){
-        straightRunning_->run(a_);
+        colorid_t nowColor = COLOR_NONE;
+        switch(statas_){
+
+        //星取取得
+        case Statas::HOSHITORI:
+            nowColor = colorDetection_->getResult();
+            if(nowColor == COLOR_BLUE || nowColor == COLOR_RED ||
+                nowColor == COLOR_GREEN || nowColor == COLOR_YELLOW){
+                hoshitori_ = nowColor;//星取保存
+                statas_ = Statas::TURN;
+            }
+            break;
+
+        //左に90度旋回
+        case Statas::TURN:
+            if(pivotTurn_->turn(90)){
+                distanceMeasurement_->setTargetDistance(200);
+                distanceMeasurement_->startMeasurement();
+                statas_ = Statas::STRAIGHT;
+            }
+            break;
+
+        //ラインに近づくように直進走行
+        case Statas::STRAIGHT:
+            straightRunning_->run(30);
+            if(distanceMeasurement_->getResult()){
+                distanceMeasurement_->setTargetDistance(1000);
+                distanceMeasurement_->startMeasurement();
+                statas_ = Statas::LINE_TRACE;
+            }
+            break;
+
+        //土俵手前までライントレース
+        case Statas::LINE_TRACE:
+            linetrace_->run(30);//右エッジ
+            if(distanceMeasurement_->getResult()){
+                statas_ = Statas::STOP;
+            }
+            break;
+
+        //新幹線が通り過ぎるまで停止
+        case Statas::STOP:
+            straightRunning_->run(0);
+            if(objectDetection_->getResult()){
+                statas_ = Statas::CLIMB;
+            }
+            break;
+
+        //登壇走行
+        case Statas::CLIMB:
+            if(climbingRunning_->run(50,400)){
+                statas_ = Statas::SUMO;
+            }
+            break;
+
+        // //相撲
+         case Statas::SUMO:
+        //     if(performSumo()){
+        //         distanceMeasurement_->setTargetDistance(300);
+        //         distanceMeasurement_->startMeasurement();
+        //         statas_ = Statas::GET_OF;
+        //     }
+
+        // //降壇
+         case Statas::GET_OF:
+        //     if(distanceMeasurement_->getResult()){
+        //         return true;
+        //     }
+        //     break;
+         return false;
+
+        }
+    return false;
+    }
+
+    bool ETSumoNeo::performSumo(){
+        // switch(sumoStatas){
+        // }
         return true;
     }
+
 }
--- strategy/ETSumoNeo.h
+++ strategy/ETSumoNeo.h
@@ -5,32 +5,69 @@
 #include "../drive/ExtrusionRunning.h"
 #include "../drive/ClimbingRunning.h"
 #include "../drive/StraightRunning.h"
+#include "../drive/PivotTurn.h"
 #include "../drive/LineTrace.h"
 #include "../detection/LineDetection.h"
+#include "../detection/ColorDetection.h"
+#include "../detection/ObjectDetection.h"
+#include "../measurement/DistanceMeasurement.h"
 
 namespace strategy{
     class ETSumoNeo : public IStrategy{
     private:
-        enum class Step{
-            A,
-            B
+        //ET相撲Neoの走行状態
+        enum class Statas{
+            HOSHITORI,
+            TURN,
+            STRAIGHT,
+            LINE_TRACE,
+            STOP,
+            CLIMB,
+            SUMO,
+            GET_OF
         };
 
+        //登壇後に行う相撲の走行ステップ
+        enum class SumoStatas{
+            a,
+            b
+        };
+
+        Statas statas_;
+        SumoStatas sumoStatas_;
+
+        //走行
         drive::ExtrusionRunning* extrusionRunning_;
         drive::ClimbingRunning* climbingRunning_;
         drive::StraightRunning* straightRunning_;
+        drive::PivotTurn* pivotTurn_;
         drive::LineTrace* linetrace_;
+
+        //検知
         detection::LineDetection* lineDetection_;
-        int a_;
+        detection::ColorDetection* colorDetection_;
+        detection::ObjectDetection* objectDetection_;
+
+
+
+        //自己位置
+        measurement::DistanceMeasurement* distanceMeasurement_;
+
+        //取得した星取
+        colorid_t hoshitori_;
+
     public:
         //コンストラクタ
-        ETSumoNeo(int a);
+        ETSumoNeo();
 
         /**
          * @brief ET相撲Neoを攻略する
          * @return 攻略完了：true,攻略中：false
          */
         bool capture();
+
+        bool performSumo();
+
     };
 }
 
