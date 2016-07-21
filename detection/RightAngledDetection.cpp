60ba7b6786b987a892a8a0270f4747c636606982
johta sisido <johta.sisido@gmail.com>
2016-07-21 14:06:45 +0900

    中括弧前の空白が残ってたので消した #40

--- detection/RightAngledDetection.cpp
+++ detection/RightAngledDetection.cpp
@@ -33,10 +33,10 @@ namespace detection{
            long distanceChanges = distanceHistory_[0] - distanceHistory_[start];
 
            //距離の変化が小さすぎると誤検知が多いかもしれないので枝切り
-           if (distanceChanges <= 3) {
+           if (distanceChanges <= 3){
                continue;
            }
-
+           
            //条件判定
            float changeRate = (float)brightnessChanges / (float)distanceChanges;
            if  ( changeRate >= minChangeRate ){
