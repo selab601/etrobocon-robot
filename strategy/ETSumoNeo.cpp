773dd27b260926b903bfaf96c50c7426730b02ee
Usui Kakeru <kakeru3.141592@gmail.com>
2016-07-25 18:53:20 +0900

    何度か成功しているので一時保存

--- strategy/ETSumoNeo.cpp
+++ strategy/ETSumoNeo.cpp
@@ -6,100 +6,452 @@ using namespace measurement;
 
 namespace strategy{
 
+
+    /*
+    失敗原因
+    ライントレースの開始時の位置が遠くて、ラインに触れた時に直角と認識してしまう
+    星取、土俵上の青と緑の検知が鈍い
+    下段を攻略中に尻尾がぶつかる危険性がある(旋回方向を変える予定)
+
+    リファクタリング
+    SumoPhaseをわかりやすくしたい
+    */
+
     ETSumoNeo::ETSumoNeo(){
-        linetrace_ = LineTrace::getInstance();
-        straightRunning_ = new StraightRunning();
-        climbingRunning_ = new ClimbingRunning();
-        extrusionRunning_ = new ExtrusionRunning();
-        pivotTurn_ = new PivotTurn();
-        lineDetection_ = new LineDetection();
-        colorDetection_ = new ColorDetection();
-        objectDetection_ = new ObjectDetection();
-        distanceMeasurement_ = new DistanceMeasurement();
-        statas_ = Statas::HOSHITORI;
-        //sumoStatas = SmoStatas:: ;
+        linetrace_              = LineTrace::getInstance();
+        straightRunning_        = new StraightRunning();
+        climbingRunning_        = new ClimbingRunning();
+        extrusionRunning_       = new ExtrusionRunning();
+        pivotTurn_              = new PivotTurn();
+        curveRunning_           = new CurveRunning();
+        lineDetection_          = new LineDetection();
+        colorDetection_         = new ColorDetection();
+        objectDetection_        = new ObjectDetection();
+        rightAngledDetection_   = new RightAngledDetection();
+        timeMeasurement_        = new TimeMeasurement();
+        distanceMeasurement_    = new DistanceMeasurement();
+
+        hasExecutedSumoPhase_   = false;
+
+        hoshitori_              = Hoshitori::NONE;
+        strategyPhase_          = StrategyPhase::HOSHITORI;
+        extrusionPhase_         = ExtrusionPhase::LINE_TRACE_1;
+        //sumoPhase_              = SumoPhase::EXTRUSION_1;
     }
 
     bool ETSumoNeo::capture(){
-        colorid_t nowColor = COLOR_NONE;
-        switch(statas_){
+        switch(strategyPhase_){
 
         //星取取得
-        case Statas::HOSHITORI:
-            nowColor = colorDetection_->getResult();
-            if(nowColor == COLOR_BLUE || nowColor == COLOR_RED ||
-                nowColor == COLOR_GREEN || nowColor == COLOR_YELLOW){
-                hoshitori_ = nowColor;//星取保存
-                statas_ = Statas::TURN;
+        //黒よりにゆっくりライントレースすることで確実に色を取得する
+        case StrategyPhase::HOSHITORI:
+            linetrace_->run(20,LineTraceEdge::RIGHT,0.5);
+            if(hoshitoriDetection(true)){
+                strategyPhase_ = StrategyPhase::TURN_1;
             }
             break;
 
-        //左に90度旋回
-        case Statas::TURN:
-            if(pivotTurn_->turn(90)){
-                distanceMeasurement_->setTargetDistance(200);
-                distanceMeasurement_->startMeasurement();
-                statas_ = Statas::STRAIGHT;
+        //左に100度旋回(90度だとラインから少し遠い)
+        case StrategyPhase::TURN_1:
+            if(pivotTurn_->turn(100)){
+                strategyPhase_ = StrategyPhase::STRAIGHT;
             }
             break;
 
         //ラインに近づくように直進走行
-        case Statas::STRAIGHT:
+        case StrategyPhase::STRAIGHT:
+            if(!hasExecutedSumoPhase_){
+                distanceMeasurement_->setTargetDistance(200);//ラインに近づく距離
+                distanceMeasurement_->startMeasurement();
+                hasExecutedSumoPhase_ = true;
+            }
             straightRunning_->run(30);
             if(distanceMeasurement_->getResult()){
-                distanceMeasurement_->setTargetDistance(1000);
-                distanceMeasurement_->startMeasurement();
-                statas_ = Statas::LINE_TRACE;
+                strategyPhase_ = StrategyPhase::LINE_TRACE;
+                hasExecutedSumoPhase_ = false;
             }
             break;
 
         //土俵手前までライントレース
-        case Statas::LINE_TRACE:
-            linetrace_->run(30);//右エッジ
+        case StrategyPhase::LINE_TRACE:
+            if(!hasExecutedSumoPhase_){
+                distanceMeasurement_->setTargetDistance(1000);//土俵手前までの距離
+                distanceMeasurement_->startMeasurement();
+                hasExecutedSumoPhase_ = true;
+            }
+            linetrace_->run(30,LineTraceEdge::RIGHT);
             if(distanceMeasurement_->getResult()){
-                statas_ = Statas::STOP;
+                strategyPhase_ = StrategyPhase::STOP_1;
+                hasExecutedSumoPhase_ = false;
             }
             break;
 
-        //新幹線が通り過ぎるまで停止
-        case Statas::STOP:
+        //新幹線を検知するまで停止
+        case StrategyPhase::STOP_1:
             straightRunning_->run(0);
             if(objectDetection_->getResult()){
-                statas_ = Statas::CLIMB;
+                ev3_speaker_play_tone ( 500, 100);//音を出す
+                strategyPhase_ = StrategyPhase::WAIT_1;
+            }
+            break;
+
+        //通り過ぎてから1秒間待つ
+        case StrategyPhase::WAIT_1:
+            if(!hasExecutedSumoPhase_){
+                timeMeasurement_->setBaseTime();
+                timeMeasurement_->setTargetTime(1000);//1秒
+                hasExecutedSumoPhase_ = true;
+            }
+            straightRunning_->run(0);
+            if(timeMeasurement_->getResult()){
+                strategyPhase_ = StrategyPhase::TURN_2;
+                hasExecutedSumoPhase_ = false;
+            }
+            break;
+
+        //登壇後の動作を安定させるため少し旋回
+        case StrategyPhase::TURN_2:
+            int turnAngle;
+            //星取が青と黄のときは赤から押し出すので左寄りへ
+            if(hoshitori_ == Hoshitori::BLUE ||
+                hoshitori_ == Hoshitori::YELLOW){
+                turnAngle = 6;
+            }else{
+                turnAngle = -6;
+            }
+            if(pivotTurn_->turn(turnAngle)){
+                strategyPhase_ = StrategyPhase::CLIMB;
             }
             break;
 
         //登壇走行
-        case Statas::CLIMB:
-            if(climbingRunning_->run(50,400)){
-                statas_ = Statas::SUMO;
+        case StrategyPhase::CLIMB:
+            if(climbingRunning_->run(40,440)){
+                strategyPhase_ = StrategyPhase::WAIT_2;
             }
             break;
 
-        // //相撲
-         case Statas::SUMO:
-        //     if(performSumo()){
-        //         distanceMeasurement_->setTargetDistance(300);
-        //         distanceMeasurement_->startMeasurement();
-        //         statas_ = Statas::GET_OF;
-        //     }
+        //登壇後に機体がぶれるので安定するまで待つ
+        case StrategyPhase::WAIT_2:
+            if(!hasExecutedSumoPhase_){
+                timeMeasurement_->setBaseTime();
+                timeMeasurement_->setTargetTime(1000);
+                hasExecutedSumoPhase_ = true;
+            }
+            straightRunning_->run(0);
+            if(timeMeasurement_->getResult()){
+                strategyPhase_ = StrategyPhase::TURN_3;
+                 hasExecutedSumoPhase_ = false;
+            }
+            break;
 
-        // //降壇
-         case Statas::GET_OF:
-        //     if(distanceMeasurement_->getResult()){
-        //         return true;
-        //     }
-        //     break;
-         return false;
+        //ラインに近づくように旋回
+        case StrategyPhase::TURN_3:
+            int turnAngle2;
+            if(hoshitori_ == Hoshitori::BLUE ||
+                hoshitori_ == Hoshitori::YELLOW){
+                turnAngle2 = 70;
+            }else{
+                turnAngle2 = -70;
+            }
+            if(pivotTurn_->turn(turnAngle2)){
+                strategyPhase_ = StrategyPhase::TURN_4;
+            }
+            break;
 
+        //ライン検知するまで旋回
+        case StrategyPhase::TURN_4:
+            int rSpeed,lSpeed;
+            if(hoshitori_ == Hoshitori::BLUE||
+                hoshitori_ == Hoshitori::YELLOW){
+                rSpeed = 15;
+                lSpeed = -rSpeed;
+            }else{
+                rSpeed = -15;
+                lSpeed = -rSpeed;
+            }
+            curveRunning_->run(rSpeed,lSpeed);
+            if(lineDetection_->getResult()){
+                strategyPhase_ = StrategyPhase::SUMO;
+            }
+
+         //相撲
+         case StrategyPhase::SUMO:
+            if(!hasExecutedSumoPhase_){
+                setProcedure();
+                hasExecutedSumoPhase_ = true;
+            }
+            if(executeSumo(sumoProcedure_.front())){
+                sumoProcedure_.front();
+                hasExecutedSumoPhase_ = false;
+            }
+            if(sumoProcedure_.empty()){//最後まで終わったら
+                strategyPhase_ = StrategyPhase::STOP_2;
+            }
+            break;
+
+        //新幹線が通り過ぎるまで停止
+        case StrategyPhase::STOP_2:
+            straightRunning_->run(0);
+            if(objectDetection_->getResult()){
+                strategyPhase_ = StrategyPhase::WAIT_3;
+                ev3_speaker_play_tone(500,100);//音を出す
+            }
+            break;
+
+        //検知した後すこし待つ
+        case StrategyPhase::WAIT_3:
+            if(!hasExecutedSumoPhase_){
+                timeMeasurement_->setBaseTime();
+                timeMeasurement_->setTargetTime(1000);
+                hasExecutedSumoPhase_ = true;
+            }
+            straightRunning_->run(0);
+            if(timeMeasurement_->getResult()){
+                strategyPhase_ = StrategyPhase::GET_OF;
+                hasExecutedSumoPhase_ = false;
+            }
+
+        //降壇
+        case StrategyPhase::GET_OF:
+            if(!hasExecutedSumoPhase_){
+                distanceMeasurement_->setTargetDistance(300);
+                distanceMeasurement_->startMeasurement();
+                hasExecutedSumoPhase_ = true;
+            }
+            straightRunning_->run(30);
+            if(distanceMeasurement_->getResult()){
+                hasExecutedSumoPhase_ = false;
+                return true;
+            }
+            break;
         }
-    return false;
+        return false;
     }
 
-    bool ETSumoNeo::performSumo(){
-        // switch(sumoStatas){
-        // }
-        return true;
+    //読み取った星取りによって押し出す順番を決める
+    void ETSumoNeo::setProcedure(){
+        //下段(赤,青)以外を押し出す場合
+        if(hoshitori_ == Hoshitori::RED || hoshitori_ == Hoshitori::BLUE){
+            sumoProcedure_ = {
+                SumoPhase::EXTRUSION_FIRST,
+                SumoPhase::ACROSS_LINE,
+                SumoPhase::TURN_1,
+                SumoPhase::UPPER_STAGE,
+                SumoPhase::ACROSS_LINE,
+                SumoPhase::TURN_2,
+                SumoPhase::EXTRUSION_SECOND,
+                SumoPhase::ACROSS_LINE,
+                SumoPhase::EXTRUSION_THIRD,
+                SumoPhase::TURN_1
+            };
+
+            //赤の場合
+            if(hoshitori_ == Hoshitori::RED){
+                firstWrestlerColor_ = Hoshitori::BLUE;
+                secondWrestlerColor_ = Hoshitori::GREEN;
+                thirdWrestlerColor_ = Hoshitori::YELLOW;
+                upperStageEdge_ = LineTraceEdge::LEFT;
+                angleTowardTop_ = -85;
+                angleTowardWrestler_ = 85;
+
+            //青の場合
+            }else{
+                firstWrestlerColor_ = Hoshitori::RED;
+                secondWrestlerColor_ = Hoshitori::YELLOW;
+                thirdWrestlerColor_ = Hoshitori::GREEN;
+                upperStageEdge_ = LineTraceEdge::RIGHT;
+                angleTowardTop_ = 85;
+                angleTowardWrestler_ = -85;
+            }
+
+        //上段(黄,緑)以外を押し出す場合
+        }else{
+            sumoProcedure_ = {
+                SumoPhase::EXTRUSION_FIRST,
+                SumoPhase::ACROSS_LINE,
+                SumoPhase::EXTRUSION_SECOND,
+                SumoPhase::ACROSS_LINE,
+                SumoPhase::TURN_1,
+                SumoPhase::UPPER_STAGE,
+                SumoPhase::ACROSS_LINE,
+                SumoPhase::TURN_2,
+                SumoPhase::EXTRUSION_THIRD,
+                SumoPhase::TURN_1
+            };
+
+            //黄の場合
+            if(hoshitori_ == Hoshitori::YELLOW){
+                firstWrestlerColor_ = Hoshitori::RED;
+                secondWrestlerColor_ = Hoshitori::BLUE;
+                thirdWrestlerColor_ = Hoshitori::GREEN;
+                upperStageEdge_ = LineTraceEdge::RIGHT;
+                angleTowardTop_ = -85;
+                angleTowardWrestler_ = -85;
+
+            //緑の場合
+            }else{
+                firstWrestlerColor_ = Hoshitori::BLUE;
+                secondWrestlerColor_ = Hoshitori::RED;
+                thirdWrestlerColor_ = Hoshitori::YELLOW;
+                upperStageEdge_ = LineTraceEdge::LEFT;
+                angleTowardTop_ = 85;
+                angleTowardWrestler_ = 85;
+            }
+        }
+    }
+
+    //相撲...登壇後から降壇前まで
+    bool ETSumoNeo::executeSumo(SumoPhase sumoPhase){
+        switch(sumoPhase){
+
+        //一人目の力士を押し出す
+        case SumoPhase::EXTRUSION_FIRST:
+            return extrusion(firstWrestlerColor_);
+
+        //二人目の力士を押し出す
+        case SumoPhase::EXTRUSION_SECOND:
+            return extrusion(secondWrestlerColor_);
+
+        //三人目の力士を押し出す
+        case SumoPhase::EXTRUSION_THIRD:
+            return extrusion(thirdWrestlerColor_);
+
+        //直角をまたぐ走行
+        case SumoPhase::ACROSS_LINE:
+            if(!hasExecutedSumoPhase_){
+                distanceMeasurement_->setTargetDistance(50);
+                distanceMeasurement_->startMeasurement();
+                hasExecutedSumoPhase_ = true;
+            }
+            straightRunning_->run(20);
+            return distanceMeasurement_->getResult();
+
+
+        //上(後段する方向)を向くように回転
+        case SumoPhase::TURN_1:
+            return pivotTurn_->turn(angleTowardTop_);
+
+        //力士を向くように回転
+        case SumoPhase::TURN_2:
+            return pivotTurn_->turn(angleTowardWrestler_);
+
+        //上段までライントレース
+        case SumoPhase::UPPER_STAGE:
+            linetrace_->run(20,upperStageEdge_);
+            return rightAngledDetection_->getResult(4.0);
+
+        }
+        return false;
+    }
+
+    //中央線から力士を押し出して戻って来る
+    bool ETSumoNeo::extrusion(Hoshitori hoshitori){
+        int rSpeed;     //右タイヤスピード
+        int lSpeed;     //左タイヤスピード
+        //int turnAngle1 = 0; //旋回角度
+        int turnAngle2;
+        LineTraceEdge edge,edge2;
+        //左側(赤,黄)と右側(青,緑)それぞれ同じ動作をする
+        switch(hoshitori){
+        //左側
+        case Hoshitori::RED:
+        case Hoshitori::YELLOW:
+            rSpeed = 15;
+            lSpeed = -rSpeed;
+            //turnAngle1 = 70;
+            turnAngle2 = -170;
+            edge = LineTraceEdge::RIGHT;
+            edge2 = LineTraceEdge::LEFT;
+            break;
+
+        //右側
+        case Hoshitori::BLUE:
+        case Hoshitori::GREEN:
+            rSpeed = -15;
+            lSpeed = -rSpeed;
+            //turnAngle1 = -70;
+            turnAngle2 = 170;
+
+            edge = LineTraceEdge::LEFT;
+            edge2 = LineTraceEdge::RIGHT;
+            break;
+
+        default: return false;
+        }
+
+        switch(extrusionPhase_){
+        //力士までライントレース
+        case ExtrusionPhase::LINE_TRACE_1:
+            linetrace_->run(15,edge);
+            if(hoshitoriDetection()){
+                extrusionPhase_ = ExtrusionPhase::EXTRUSION;
+            }
+            break;
+
+        //押し出し走行
+        case ExtrusionPhase::EXTRUSION:
+            if(extrusionRunning_->run(SUMO_EXTRUSION_SPEED,SUMO_EXTRUSION_DISTANCE)){
+                extrusionPhase_ = ExtrusionPhase::TURN_1;
+            }
+            break;
+
+        //星取を誤検知しないように170度旋回しておきたい
+        case ExtrusionPhase::TURN_1:
+            if(pivotTurn_->turn(turnAngle2)){
+                extrusionPhase_ = ExtrusionPhase::TURN_2;
+            }
+            break;
+
+        //ラインまで旋回
+        case ExtrusionPhase::TURN_2:
+            curveRunning_->run(-rSpeed,-lSpeed);
+            if(lineDetection_->getResult()){
+                timeMeasurement_->setBaseTime();
+                timeMeasurement_->setTargetTime(1000);
+                extrusionPhase_ = ExtrusionPhase::LINE_TRACE_2;
+            }
+            break;
+
+        //直角までライントレース
+        case ExtrusionPhase::LINE_TRACE_2:
+            linetrace_->run(20,edge2);
+            //直角誤検知しないように
+            if(!timeMeasurement_->getResult()){
+                break;
+            }
+            if(rightAngledDetection_->getResult()){
+                extrusionPhase_ = ExtrusionPhase::LINE_TRACE_1;//状態を戻しておく
+                return true;
+            }
+            break;
+        }
+        return false;
+    }
+
+    bool ETSumoNeo::hoshitoriDetection(bool saveHoshitori){
+        // colorid_t nowColor = COLOR_NONE;
+        colorid_t nowColor = colorDetection_->getResult();
+        if(nowColor == COLOR_BLUE || nowColor == COLOR_RED ||
+        nowColor == COLOR_GREEN || nowColor == COLOR_YELLOW){
+            //星取保存
+            if(saveHoshitori){
+                switch(nowColor){
+                case COLOR_RED:
+                    hoshitori_ = Hoshitori::RED; break;
+                case COLOR_BLUE:
+                    hoshitori_ = Hoshitori::BLUE; break;
+                case COLOR_YELLOW:
+                    hoshitori_ = Hoshitori::YELLOW; break;
+                case COLOR_GREEN:
+                    hoshitori_ = Hoshitori::GREEN; break;
+                default: return false;
+                }
+            }
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
--- strategy/ETSumoNeo.h
+++ strategy/ETSumoNeo.h
@@ -7,54 +7,107 @@
 #include "../drive/StraightRunning.h"
 #include "../drive/PivotTurn.h"
 #include "../drive/LineTrace.h"
+#include "../drive/CurveRunning.h"
 #include "../detection/LineDetection.h"
 #include "../detection/ColorDetection.h"
 #include "../detection/ObjectDetection.h"
+#include "../detection/RightAngledDetection.h"
+#include "../measurement/TimeMeasurement.h"
 #include "../measurement/DistanceMeasurement.h"
+#include <vector>
+
+#define SUMO_EXTRUSION_DISTANCE  100 //押し出し走行距離
+#define SUMO_EXTRUSION_SPEED     20  //押し出しスピード
 
 namespace strategy{
     class ETSumoNeo : public IStrategy{
     private:
         //ET相撲Neoの走行状態
-        enum class Statas{
-            HOSHITORI,
-            TURN,
-            STRAIGHT,
-            LINE_TRACE,
-            STOP,
-            CLIMB,
-            SUMO,
-            GET_OF
+        enum class StrategyPhase{
+            HOSHITORI,  //星取り取得
+            TURN_1,     //
+            STRAIGHT,   //
+            LINE_TRACE, //
+            STOP_1,     //
+            WAIT_1,     //
+            CLIMB,      //
+            WAIT_2,     //
+            TURN_2,     //
+            TURN_3,     //
+            TURN_4,
+            SUMO,       //
+            STOP_2,     //
+            WAIT_3,     //
+            GET_OF      //
         };
 
-        //登壇後に行う相撲の走行ステップ
-        enum class SumoStatas{
-            a,
-            b
+        //相撲全体の状態
+        enum class SumoPhase{
+            EXTRUSION_FIRST,    //
+            EXTRUSION_SECOND,    //
+            EXTRUSION_THIRD,    //
+            TURN_1,         //
+            TURN_2,         //
+            UPPER_STAGE,    //
+            ACROSS_LINE     //
         };
 
-        Statas statas_;
-        SumoStatas sumoStatas_;
+        //特定の力士を押し出す走行状態
+        enum class ExtrusionPhase{
+            //TURN_0,
+            LINE_TRACE_1,   //
+            EXTRUSION,      //
+            TURN_1,         //
+            TURN_2,         //
+            LINE_TRACE_2    //
+        };
+
+        //星取
+        enum class Hoshitori{
+            RED,        //赤
+            BLUE,       //青
+            YELLOW,     //黄
+            GREEN,      //緑
+            NONE        //未定義
+        };
+
+        std::vector<SumoPhase> sumoProcedure_;
+
+
+        StrategyPhase strategyPhase_;
+        ExtrusionPhase extrusionPhase_;
+        //SumoPhase sumoPhase_;
+        Hoshitori hoshitori_;
 
         //走行
         drive::ExtrusionRunning* extrusionRunning_;
         drive::ClimbingRunning* climbingRunning_;
         drive::StraightRunning* straightRunning_;
         drive::PivotTurn* pivotTurn_;
+        drive::CurveRunning* curveRunning_;
         drive::LineTrace* linetrace_;
 
         //検知
         detection::LineDetection* lineDetection_;
         detection::ColorDetection* colorDetection_;
         detection::ObjectDetection* objectDetection_;
+        detection::RightAngledDetection* rightAngledDetection_;
 
-
-
-        //自己位置
+        //計測
         measurement::DistanceMeasurement* distanceMeasurement_;
+        measurement::TimeMeasurement* timeMeasurement_;
 
-        //取得した星取
-        colorid_t hoshitori_;
+
+        bool hasExecutedSumoPhase_;
+
+        //押し出す力士の色
+        Hoshitori firstWrestlerColor_;
+        Hoshitori secondWrestlerColor_;
+        Hoshitori thirdWrestlerColor_;
+
+        int angleTowardTop_;             //上に向かう角度
+        int angleTowardWrestler_;        //力士に向かう角度
+        drive::LineTraceEdge upperStageEdge_;   //上段に向かうライントレースのエッジ
 
     public:
         //コンストラクタ
@@ -66,13 +119,36 @@ namespace strategy{
          */
         bool capture();
 
-        bool performSumo();
+    private:
+        /**
+         * @brief 相撲を行う
+         * @details 星取の色によって振る舞いを変える
+         * @return 相撲終了:true,攻略中:false
+         */
+        bool executeSumo(SumoPhase sumoPhase);
 
+        void setProcedure();
+
+        /**
+         * @brief 指定した星取の力士を押し出す
+         * @details 中央線からスタートし、中央線で終了する、
+         * 左側(赤・黄)と右側(青・緑)はそれぞれ同じ動作をする
+         *
+         * @param hoshitori 押し出す力士の星取
+         * @return 押し出し終了:true,攻略中:false
+         */
+        bool extrusion(Hoshitori hoshitori);
+
+        /**
+         * @brief 星取検知
+         * @details 星取の赤、青、黄、緑のいずれかを検知する、
+         * 引数をtureとすることで取得した星取を保存できる
+         *
+         * @param getHoshitori 星取を保存するかどうか
+         * @return 赤、青、黄、緑だった場合:true
+         */
+        bool hoshitoriDetection(bool saveHoshitori = false);
     };
 }
-
-
-
-
 
 #endif
