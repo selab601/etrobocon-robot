583d2a0036f8c14077f06d0a6e0496ded0f609cd
NakayamaKazuki <13t4051g@vc.ibaraki.ac.jp>
2016-07-28 14:14:01 +0900

    インデント修正

--- drive/Avoidance.cpp
+++ drive/Avoidance.cpp
@@ -1,177 +1,177 @@
 #include "Avoidance.h"
 
 namespace drive{
-	//コンストラクタ
-	Avoidance::Avoidance(){
-		curveRunning_ = new CurveRunning();
-		straightRunning_ = new StraightRunning();
-		lineDetection_ = new detection::LineDetection();
+    //コンストラクタ
+    Avoidance::Avoidance(){
+        curveRunning_ = new CurveRunning();
+        straightRunning_ = new StraightRunning();
+        lineDetection_ = new detection::LineDetection();
 
-		bodyAngle_ = new measurement::BodyAngleMeasurement();
-		state_ = State::START;
-	}
-	bool Avoidance::startAvoidance(DirectionKind kind){
-		switch (kind) {
-		case DirectionKind::RIGHT:
-			return(this->right());
-		case DirectionKind::LEFT:
-			return(this->left());
-		case DirectionKind::STRAIGHT_RIGHT:
-			return(this->straightRight());
-		case DirectionKind::STRAIGHT_LEFT:
-			return(this->straightLeft());
+        bodyAngle_ = new measurement::BodyAngleMeasurement();
+        state_ = State::START;
+    }
+    bool Avoidance::startAvoidance(DirectionKind kind){
+        switch (kind) {
+        case DirectionKind::RIGHT:
+            return(this->right());
+        case DirectionKind::LEFT:
+            return(this->left());
+        case DirectionKind::STRAIGHT_RIGHT:
+            return(this->straightRight());
+        case DirectionKind::STRAIGHT_LEFT:
+            return(this->straightLeft());
         default:
             return false;
-		}
-	}
-	bool Avoidance::right(){
-		switch(state_){
-		case State::START:
-			bodyAngle_->setBaseAngle();
-			state_ = State::TURN_RIGHT;
-			break;
-		case State::TURN_RIGHT:
-			curveRunning_->run(5,30);
-			if(bodyAngle_->getResult() <= -45){
-				state_ = State::STRAIGHT_1;
-			}
-			break;
-		case State::STRAIGHT_1:
-			straightRunning_->run(20);
-			if(lineDetection_->getResult(20)){
+        }
+    }
+    bool Avoidance::right(){
+        switch(state_){
+        case State::START:
+            bodyAngle_->setBaseAngle();
+            state_ = State::TURN_RIGHT;
+            break;
+        case State::TURN_RIGHT:
+            curveRunning_->run(5,30);
+            if(bodyAngle_->getResult() <= -45){
+                state_ = State::STRAIGHT_1;
+            }
+            break;
+        case State::STRAIGHT_1:
+            straightRunning_->run(20);
+            if(lineDetection_->getResult(20)){
                 straightRunning_->run(0);
-				state_ = State::LINE_RETURN;
-			}
-			break;
-		case State::LINE_RETURN:
-			curveRunning_->run(5,30);
-			if(bodyAngle_->getResult() <= -90){
-				state_ = State::START;
-				straightRunning_->run(0);
-				return true;
-			}
+                state_ = State::LINE_RETURN;
+            }
+            break;
+        case State::LINE_RETURN:
+            curveRunning_->run(5,30);
+            if(bodyAngle_->getResult() <= -90){
+                state_ = State::START;
+                straightRunning_->run(0);
+                return true;
+            }
             break;
         default :
             return false;
-		}
+        }
         return false;
-	}
-	bool Avoidance::left(){
-		switch(state_){
-		case State::START:
-			bodyAngle_->setBaseAngle();
-			state_ = State::TURN_LEFT;
-			break;
-		case State::TURN_LEFT:
-			curveRunning_->run(30,5);
-			if(bodyAngle_->getResult() >= 45){
-				state_ = State::STRAIGHT_1;
-			}
-			break;
-		case State::STRAIGHT_1:
-			straightRunning_->run(20);
-			if(lineDetection_->getResult(20)){
-				state_ = State::LINE_RETURN;
-			}
-			break;
-		case State::LINE_RETURN:
-			curveRunning_->run(30,5);
-			if(bodyAngle_->getResult() >= 90){
-				state_ = State::START;
-				straightRunning_->run(0);
-				return true;
-			}
+    }
+    bool Avoidance::left(){
+        switch(state_){
+        case State::START:
+            bodyAngle_->setBaseAngle();
+            state_ = State::TURN_LEFT;
+            break;
+        case State::TURN_LEFT:
+            curveRunning_->run(30,5);
+            if(bodyAngle_->getResult() >= 45){
+                state_ = State::STRAIGHT_1;
+            }
+            break;
+        case State::STRAIGHT_1:
+            straightRunning_->run(20);
+            if(lineDetection_->getResult(20)){
+                state_ = State::LINE_RETURN;
+            }
+            break;
+        case State::LINE_RETURN:
+            curveRunning_->run(30,5);
+            if(bodyAngle_->getResult() >= 90){
+                state_ = State::START;
+                straightRunning_->run(0);
+                return true;
+            }
             break;
         default:
-			return false;
-		}
+            return false;
+        }
         return false;
-	}
-	bool Avoidance::straightRight(){
-		switch(state_){
-		case State::START:
-			bodyAngle_->setBaseAngle();
-			state_ = State::TURN_RIGHT;
-			break;
-		case State::TURN_RIGHT:
-			curveRunning_->run(5,30);
-			if(bodyAngle_->getResult() <= -40){
-				state_ = State::STRAIGHT_1;
-			}
-			break;
-		case State::STRAIGHT_1:
-			straightRunning_->run(20);
-			if(lineDetection_->getResult(20)){
-				state_ = State::TURN_LEFT;
-			}
-			break;
-		case State::TURN_LEFT:
-			curveRunning_->run(30,5);
-			if(bodyAngle_->getResult() >= 40){
-				state_ = State::STRAIGHT_2;
-			}
-			break;
-		case State::STRAIGHT_2:
-			straightRunning_->run(20);
-			if(lineDetection_->getResult(20)){
-				state_ = State::LINE_RETURN;
-			}
-			break;
-		case State::LINE_RETURN:
-			curveRunning_->run(5,30);
-			if(bodyAngle_->getResult() <= 0){
-				state_ = State::START;
-				straightRunning_->run(0);
-				return true;
-			}
+    }
+    bool Avoidance::straightRight(){
+        switch(state_){
+        case State::START:
+            bodyAngle_->setBaseAngle();
+            state_ = State::TURN_RIGHT;
+            break;
+        case State::TURN_RIGHT:
+            curveRunning_->run(5,30);
+            if(bodyAngle_->getResult() <= -40){
+                state_ = State::STRAIGHT_1;
+            }
+            break;
+        case State::STRAIGHT_1:
+            straightRunning_->run(20);
+            if(lineDetection_->getResult(20)){
+                state_ = State::TURN_LEFT;
+            }
+            break;
+        case State::TURN_LEFT:
+            curveRunning_->run(30,5);
+            if(bodyAngle_->getResult() >= 40){
+                state_ = State::STRAIGHT_2;
+            }
+            break;
+        case State::STRAIGHT_2:
+            straightRunning_->run(20);
+            if(lineDetection_->getResult(20)){
+                state_ = State::LINE_RETURN;
+            }
+            break;
+        case State::LINE_RETURN:
+            curveRunning_->run(5,30);
+            if(bodyAngle_->getResult() <= 0){
+                state_ = State::START;
+                straightRunning_->run(0);
+                return true;
+            }
             break;
         default:
-			return false;
-		}
+            return false;
+        }
         return false;
-	}
+    }
 
-	bool Avoidance::straightLeft(){
-		switch(state_){
-		case State::START:
-			bodyAngle_->setBaseAngle();
-			state_ = State::TURN_LEFT;
-			break;
-		case State::TURN_LEFT:
-			curveRunning_->run(30,5);
-			if(bodyAngle_->getResult() <= 35){
-				state_ = State::STRAIGHT_1;
-			}
-			break;
-		case State::STRAIGHT_1:
-			straightRunning_->run(20);
-			if(lineDetection_->getResult(20)){
-				state_ = State::TURN_RIGHT;
-			}
-			break;
-		case State::TURN_RIGHT:
-			curveRunning_->run(5,30);
-			if(bodyAngle_->getResult() >= -35){
-				state_ = State::STRAIGHT_2;
-			}
-			break;
-		case State::STRAIGHT_2:
-			straightRunning_->run(20);
-			if(lineDetection_->getResult(20)){
-				state_ = State::LINE_RETURN;
-			}
-			break;
-		case State::LINE_RETURN:
-			curveRunning_->run(30,5);
-			if(bodyAngle_->getResult() <= 0){
-				state_ = State::START;
-				straightRunning_->run(0);
-				return true;
-			}
+    bool Avoidance::straightLeft(){
+        switch(state_){
+        case State::START:
+            bodyAngle_->setBaseAngle();
+            state_ = State::TURN_LEFT;
+            break;
+        case State::TURN_LEFT:
+            curveRunning_->run(30,5);
+            if(bodyAngle_->getResult() <= 35){
+                state_ = State::STRAIGHT_1;
+            }
+            break;
+        case State::STRAIGHT_1:
+            straightRunning_->run(20);
+            if(lineDetection_->getResult(20)){
+                state_ = State::TURN_RIGHT;
+            }
+            break;
+        case State::TURN_RIGHT:
+            curveRunning_->run(5,30);
+            if(bodyAngle_->getResult() >= -35){
+                state_ = State::STRAIGHT_2;
+            }
+            break;
+        case State::STRAIGHT_2:
+            straightRunning_->run(20);
+            if(lineDetection_->getResult(20)){
+                state_ = State::LINE_RETURN;
+            }
+            break;
+        case State::LINE_RETURN:
+            curveRunning_->run(30,5);
+            if(bodyAngle_->getResult() <= 0){
+                state_ = State::START;
+                straightRunning_->run(0);
+                return true;
+            }
             break;
         default:
-			return false;
-		}
+            return false;
+        }
         return false;
-	}
+    }
 }
--- drive/Avoidance.h
+++ drive/Avoidance.h
@@ -8,80 +8,80 @@
 #include "ev3api.h"
 
 namespace drive{
-	/**
-	 * @brief 台座の避け方の方向の種類
-	 **/
-	enum class DirectionKind{
-		RIGHT,
-		LEFT,
-		STRAIGHT_RIGHT,
-		STRAIGHT_LEFT
-	};
+    /**
+     * @brief 台座の避け方の方向の種類
+     **/
+    enum class DirectionKind{
+        RIGHT,
+        LEFT,
+        STRAIGHT_RIGHT,
+        STRAIGHT_LEFT
+    };
 
-	/**
-	 * @brief 現在の走行状態
-	 **/
-	enum class State{
-		START,
-		TURN_RIGHT,
-		TURN_LEFT,
-		STRAIGHT_1,
-		STRAIGHT_2,
+    /**
+     * @brief 現在の走行状態
+     **/
+    enum class State{
+        START,
+        TURN_RIGHT,
+        TURN_LEFT,
+        STRAIGHT_1,
+        STRAIGHT_2,
         LINE_RETURN
-	};
-	/**
-	 * @brief 台座回避走行クラス
-	 **/
-	class Avoidance{
-	private:
-		CurveRunning* curveRunning_;
-		StraightRunning* straightRunning_;
-		detection::LineDetection* lineDetection_;
-		measurement::BodyAngleMeasurement* bodyAngle_;
-		State state_;
+    };
+    /**
+     * @brief 台座回避走行クラス
+     **/
+    class Avoidance{
+    private:
+        CurveRunning* curveRunning_;
+        StraightRunning* straightRunning_;
+        detection::LineDetection* lineDetection_;
+        measurement::BodyAngleMeasurement* bodyAngle_;
+        State state_;
 
-	public:
-		//コンストラクタ
-		Avoidance();
+    public:
+        //コンストラクタ
+        Avoidance();
 
-		/**
-		 * @brief 台座回避を実行する
+        /**
+         * @brief 台座回避を実行する
          * @param 台座の避け方の方向の種類
          * @return 回避走行が終了したらtrueを返す
-		 **/
-		bool startAvoidance(DirectionKind KIND);
+         **/
+        bool startAvoidance(DirectionKind KIND);
 
-		/**
-		 * @brief 右折しながら台座回避
+        /**
+         * @brief 右折しながら台座回避
          * @brief スタート位置はブロックか3cm以上離れていれば成功する
          * @brief 終了地点は前で実行するほど後ろで終了する
          * @return 回避走行が終了したらtrueを返す
-		 **/
-		bool right();
+         **/
+        bool right();
 
-		 /**
-		 * @brief 左折しながら台座回避
+         /**
+         * @brief 左折しながら台座回避
          * @brief スタート位置はブロックか3cm以上離れていれば成功する
          * @brief 終了地点は前で実行するほど後ろで終了する
          * @return 回避走行が終了したらtrueを返す
-		 **/
-		bool left();
+         **/
+        bool left();
 
-		 /**
-		 * @brief 直進、右から回り込んで台座回避
+         /**
+         * @brief 直進、右から回り込んで台座回避
          * @brief スタート位置はブロックか3cm以上離れていれば成功する
          * @return 回避走行が終了したらtrueを返す
-		 **/
-		bool straightRight();
+         **/
+        bool straightRight();
 
-		 /**
-		 * @brief 直進、左から回り込んで台座回避
+         /**
+         * @brief 直進、左から回り込んで台座回避
          * @brief スタート位置はブロックか3cm以上離れていれば成功する
          * @return 回避走行が終了したらtrueを返す
-		 **/
-		bool straightLeft();
+         **/
+        bool straightLeft();
 
-	};
+    };
 
 }
 
