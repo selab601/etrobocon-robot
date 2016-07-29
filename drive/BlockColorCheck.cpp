50f844374eea6bb81e087c0e52f844554e0c4b24
Tasuku Tozawa <kamuhata.you@gmail.com>
2016-07-29 16:24:50 +0900

    コメントを追加

--- drive/BlockColorCheck.cpp
+++ drive/BlockColorCheck.cpp
@@ -2,17 +2,22 @@
 
 namespace drive {
     BlockColorCheck::BlockColorCheck() {
+        // キューに状態を順番に詰める．
+        // 今のところは以下の順序
+        //   1. table(台座)検知&色取得 (DetectTableState)
+        //   2. block検知&色取得 (DetectBlockState)
+        // ただし，最初の手順は直接 executingState_ (現在実行中の状態) に格納する
         executingState_ = new DetectTableState();
         states_.push(new DetectBlockState());
     }
 
     bool BlockColorCheck::isExecuted(colorset_t* result) {
-        // 実行中
+        // 現在の状態(executingState_)実行中
         if (executingState_->isExecuted(result) == false) {
             return false;
         }
 
-        // 実行終了
+        // 現在の状態実行終了
         if (states_.empty()) {
             return true;
         } else {
