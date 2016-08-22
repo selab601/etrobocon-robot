f90cf237db552415e8f317ffd513f7486d9a4aeb
Tasuku Tozawa <15nm722x@vc.ibaraki.ac.jp>
2016-08-22 14:13:14 +0900

    変数名が間違っていたので修正

--- detection/ColorDetection.cpp
+++ detection/ColorDetection.cpp
@@ -13,7 +13,7 @@ namespace detection{
         greenPerRed_ = rgbColor_.g*(1.0)/(rgbColor_.r + 0.01);  //! r値とg値の比
         bluePerRed_ = rgbColor_.b*(1.0)/(rgbColor_.r + 0.01);   //! r値とb値の比
 
-        hsv_raw_t hsv = detection::ColorDetection::Rgb2Hsv(aveRGB);
+        hsv_raw_t hsv = detection::ColorDetection::Rgb2Hsv(rgbColor_);
         int hueValue = hsv.h;
 
         colorid_t result;
