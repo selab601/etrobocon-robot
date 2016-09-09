4781d043ac74bf9c3404cd32b903e607132a92fe
Usui Kakeru <kakeru3.141592@gmail.com>
2016-09-09 23:59:41 +0900

    コメント追加

--- strategy/PrizeTailVer.cpp
+++ strategy/PrizeTailVer.cpp
@@ -7,96 +7,113 @@ using namespace device;
 
 namespace strategy{
     PrizeTailVer::PrizeTailVer(){
-        lineTrace_ = LineTrace::getInstance();
-        straightRunning_ = new StraightRunning();
-        pivotTurn_ = new PivotTurn();
-        curveRunning_ = new CurveRunning();
-        objectDetection_ = new ObjectDetection(100);
-        lineDetection_ = new LineDetection();
-        distanceMeasurement_  = new DistanceMeasurement();
-        timeMeasurement_ = new TimeMeasurement();
-        bodyAngleMeasurement_ = new BodyAngleMeasurement();
-        motors_ = Motors::getInstance();
-        strategySuccess_ = false;
-        hasExecutedPhase_ = false;
+        lineTrace_              = LineTrace::getInstance();
+        straightRunning_        = new StraightRunning();
+        pivotTurn_              = new PivotTurn();
+        curveRunning_           = new CurveRunning();
+        objectDetection_        = new ObjectDetection(100);
+        lineDetection_          = new LineDetection();
+        distanceMeasurement_    = new DistanceMeasurement();
+        timeMeasurement_        = new TimeMeasurement();
+        bodyAngleMeasurement_   = new BodyAngleMeasurement();
+        motors_                 = Motors::getInstance();
+        strategySuccess_        = false;
+        hasExecutedPhase_       = false;
     }
 
+    //攻略する
     bool PrizeTailVer::capture(){
         static unsigned int procedureNumber = 0;
         if(!strategySuccess_){
+            //難所攻略手順を1つする実行する
             if(executePhase(phaseProcedure_[procedureNumber])){
                 procedureNumber++;
-                hasExecutedPhase_ = false;
-                bodyAngleMeasurement_->setBaseAngle();
+                hasExecutedPhase_ = false;//フラグを戻しておく
+                bodyAngleMeasurement_->setBaseAngle();//車体角度リセット
                 ev3_speaker_play_tone ( 500, 100);//音を出す
             }
         }
+        //最後まで終わったら
         if(procedureNumber == phaseProcedure_.size()){
             strategySuccess_ = true;
         }
         return strategySuccess_;
     }
 
+    //攻略手順
     bool PrizeTailVer::executePhase(Phase phase){
         static int baceCount = 0;
         switch(phase){
-        case Phase::LINE_TRACE:
+
+        //ライントレースしながら障害物検知
+        case Phase::LINE_TRACE_UP_TO_PRIZE:
             lineTrace_->run(15,LineTraceEdge::RIGHT);
             baceCount = motors_->getCount(MOTOR_ARM);
             return objectDetection_->getResult();
 
+        //アームを下げる
         case Phase::DOWN_ARM:
             startTimeMeasurement(500);
             motors_->setPWM(MOTOR_ARM, -20);
             straightRunning_->run(0);
             return timeMeasurement_->getResult();
 
-        case Phase::PUT_IN_LOWER:
+        //懸賞の下にアームを入れる
+        case Phase::PUT_IN_LOWER_OF_PRIZE:
             startDistanceMeasurement(80);
             motors_->setPWM(MOTOR_ARM, 0);
             straightRunning_->run(10);
             return distanceMeasurement_->getResult();
 
-        case Phase::LIFT:
+        //持ち上げる
+        case Phase::LIFT_PRIZE:
             startTimeMeasurement(1000);
             motors_->setPWM(MOTOR_ARM, 30);
             straightRunning_->run(0);
             return timeMeasurement_->getResult();
 
-        case Phase::DOWN:
+        //下ろす
+        case Phase::DOWN_PRIZE:
             motors_->setPWM(MOTOR_ARM,-20);
             straightRunning_->run(0);
             return motors_->getCount(MOTOR_ARM) <= baceCount;
 
-        case Phase::BACK:
+        //すこし下がる
+        case Phase::PUT_AFTER_BACK:
             startDistanceMeasurement(50);
             straightRunning_->run(-10);
             return distanceMeasurement_->getResult();
 
-        case Phase::BACK2:
+        //
+        case Phase::CURVE_AFTER_BACK:
             startDistanceMeasurement(35);
             straightRunning_->run(-10);
             return distanceMeasurement_->getResult();
 
-        case Phase::TURN:
+        //左に90度旋回
+        case Phase::LEFT_90_ROTATION:
             return pivotTurn_->turn(90);
 
-        case Phase::CURVE:
+        //懸賞の左側にカーブで移動
+        case Phase::CURVE_UP_TO_PRIZE_SIDE:
             curveRunning_->run(10,30);
             return bodyAngleMeasurement_->getResult() <= -90;
 
-        case Phase::TAIL_MOVE:
+        //尻尾を動かす
+        case Phase::SPIN_TAIL:
             startTimeMeasurement(500);
             motors_->setPWM(MOTOR_TAIL,-50);
             straightRunning_->run(0);
             return timeMeasurement_->getResult();
 
+        //1cm進む
         case Phase::STRAIGHT_1_CM:
             startDistanceMeasurement(20);
             straightRunning_->run(10);
             return distanceMeasurement_->getResult();
 
-        case Phase::ZERO:
+        //尻尾を止める
+        case Phase::STOP_TAIL:
             motors_->setPWM(MOTOR_TAIL,0);
             return true;
 
@@ -104,6 +121,7 @@ namespace strategy{
         }
     }
 
+    //時間検知の設定をまとめたもの
     void PrizeTailVer::startTimeMeasurement(int time){
         if(!hasExecutedPhase_){
             timeMeasurement_->setBaseTime();
@@ -112,6 +130,7 @@ namespace strategy{
         }
     }
 
+    //距離検知の設定をまとめたもの
     void PrizeTailVer::startDistanceMeasurement(int distance){
         if(!hasExecutedPhase_){
             distanceMeasurement_->setTargetDistance(distance);
--- strategy/PrizeTailVer.h
+++ strategy/PrizeTailVer.h
@@ -14,57 +14,63 @@
 #include "../device/Motors.h"
 #include <vector>
 
-
-
 namespace strategy{
     class PrizeTailVer : public IStrategy{
     private:
         enum class Phase{
-            LINE_TRACE,
+            LINE_TRACE_UP_TO_PRIZE,
             DOWN_ARM,
-            PUT_IN_LOWER,
-            LIFT,
-            DOWN,
-            BACK,
-            BACK2,
-            TURN,
-            CURVE,
+            PUT_IN_LOWER_OF_PRIZE,
+            LIFT_PRIZE,
+            DOWN_PRIZE,
+            PUT_AFTER_BACK,
+            CURVE_AFTER_BACK,
+            LEFT_90_ROTATION,
+            CURVE_UP_TO_PRIZE_SIDE,
             STRAIGHT_1_CM,
-            TAIL_MOVE,
-            ZERO
+            SPIN_TAIL,
+            STOP_TAIL
         };
 
+        //懸賞運びの攻略手順
         std::vector<Phase> phaseProcedure_{
-            Phase::LINE_TRACE,
-            Phase::DOWN_ARM,
-            Phase::PUT_IN_LOWER,
-            Phase::LIFT,
-            Phase::DOWN,
-            Phase::BACK,
-            Phase::TURN,
-            Phase::CURVE,
-            Phase::BACK2,
-            Phase::TURN,
-            Phase::STRAIGHT_1_CM,
-            Phase::TAIL_MOVE,
-            Phase::ZERO
+            Phase::LINE_TRACE_UP_TO_PRIZE,//懸賞前までライントレース
+            Phase::DOWN_ARM,              //アームを下げる
+            Phase::PUT_IN_LOWER_OF_PRIZE, //懸賞の下にアームを入れる
+            Phase::LIFT_PRIZE,            //懸賞を持ち上げる
+            Phase::DOWN_PRIZE,            //懸賞を下ろす
+            Phase::PUT_AFTER_BACK,        //置いた後にバック
+            Phase::LEFT_90_ROTATION,      //左90度旋回
+            Phase::CURVE_UP_TO_PRIZE_SIDE,//懸賞の横までカーブ
+            Phase::CURVE_AFTER_BACK,      //カーブ後にバック
+            Phase::LEFT_90_ROTATION,      //左90度カーブ
+            Phase::STRAIGHT_1_CM,         //1cm進む
+            Phase::SPIN_TAIL,             //尻尾回す
+            Phase::STOP_TAIL              //尻尾止める
         };
 
+        //走行
         drive::PivotTurn* pivotTurn_;
         drive::CurveRunning* curveRunning_;
         drive::StraightRunning* straightRunning_;
         drive::LineTrace* lineTrace_;
 
+        //検知
         detection::ObjectDetection* objectDetection_;
         detection::LineDetection* lineDetection_;
 
+        //計測
         measurement::DistanceMeasurement* distanceMeasurement_;
         measurement::TimeMeasurement* timeMeasurement_;
         measurement::BodyAngleMeasurement* bodyAngleMeasurement_;
 
+        //デバイス
         device::Motors* motors_;
 
+        //難所クリアしたかどうか
         bool strategySuccess_;
+
+        //距離検知の設定などを一度だけ行うためのフラグ
         bool hasExecutedPhase_;
     public:
         //コンストラクタ
@@ -77,8 +83,28 @@ namespace strategy{
         bool capture();
 
     private:
+        /**
+         * @brief 懸賞運びの手順を実行
+         *
+         * @param phase 手順
+         * @return 終了:true,攻略中:false
+         */
         bool executePhase(Phase phase);
+
+        /**
+         * @brief 時間検知を開始する
+         * @details setBaseTime(),setTargetTime(time)を同時に行う
+         *
+         * @param time 検知する時間
+         */
         void startTimeMeasurement(int time);
+
+        /**
+         * @brief 距離検知を開始する
+         * @details setTargetDistance(distance),startMeasurement()を同時に行う
+         *
+         * @param time 検知する距離
+         */
         void startDistanceMeasurement(int distance);
 
     };
