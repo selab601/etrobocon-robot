77f1ea0fc06dd31fe7c714b4cd5a142cff436bd4
motoki nagaoka <16nm719r@vc.ibaraki.ac.jp>
2016-09-12 02:07:43 +0900

    加速度をつけて直進するメソッドを追加

--- drive/StraightRunning.cpp
+++ drive/StraightRunning.cpp
@@ -4,14 +4,77 @@
 
 #include "StraightRunning.h"
 
+using namespace device;
 namespace drive{
-	
-	//コンストラクタ
-	StraightRunning::StraightRunning(){
-		motor_	= device::Motors::getInstance();
-	}
 
-	void StraightRunning::run(int speed){
-		motor_->setWheelPWM(speed,speed);
-	}
+    //コンストラクタ
+    StraightRunning::StraightRunning(){
+        motor_	= device::Motors::getInstance();
+    }
+
+    void StraightRunning::run(int speed){
+        motor_->setWheelPWM(speed,speed);
+    }
+
+    void StraightRunning::initialize(){
+        isAccelerating_ = true;
+        changeSpeedInitialized_ = false;
+    }
+
+    void StraightRunning::run(int targetPwm, int acceleration){
+        if (isAccelerating_){
+            if (changeSpeed(targetPwm, acceleration)){
+                isAccelerating_ = false;
+            }
+        }
+        else{
+            int wheelPwm = (motor_->getPWM(MOTOR_LEFT) + motor_->getPWM(MOTOR_RIGHT)) / 2;
+            int pwmDiff = targetPwm - wheelPwm;
+            if ( (-1 <= pwmDiff) && (1 >= pwmDiff) ){
+                run(targetPwm);
+            }
+            else {
+                isAccelerating_ = true;
+            }
+        }
+    }
+
+    bool StraightRunning::changeSpeed(int targetPwm, int acceleration){
+        static ev3api::Clock clock = ev3api::Clock();
+        static long currentTimeMs = 0;
+        static double currentPwm = 0;
+        // 値の初期化
+        if ( !changeSpeedInitialized_ ){
+            changeSpeedInitialized_ = true;
+            currentTimeMs = clock.now();
+            currentPwm = (motor_->getPWM(MOTOR_LEFT) + motor_->getPWM(MOTOR_RIGHT)) / 2;
+            return false;
+        }
+
+        // 加速度を絶対値にする
+        if (acceleration < 0 ){
+            acceleration *= -1;
+        }
+
+        // 減速するときは加速度を負の値に
+        if (currentPwm > targetPwm ){
+            acceleration *= -1;
+        }
+
+        // 加速度[pwm/s] * 時間[s]
+        double diff = (double)acceleration * (double)(clock.now() - currentTimeMs) / (double)1000;
+        currentTimeMs = clock.now();
+        currentPwm += diff;
+
+        bool speedChanged = false;
+        int pwmDiff = targetPwm - currentPwm;
+        if ( (-1 <= pwmDiff) && (1 >= pwmDiff) ){
+            currentPwm = targetPwm;
+            speedChanged = true;
+            changeSpeedInitialized_ = false; // 初期化
+        }
+
+        run((int)currentPwm);
+        return speedChanged;
+    }
 }
--- drive/StraightRunning.h
+++ drive/StraightRunning.h
@@ -7,23 +7,51 @@
 
 #include "device/Motors.h"
 #include "ev3api.h"
+#include <Clock.h>
 
 namespace drive{
 
-	class StraightRunning{
+    class StraightRunning{
 
-		private:
-			device::Motors* motor_;
+        private:
+            device::Motors* motor_;
+            bool isAccelerating_ = true;
+            bool changeSpeedInitialized_ = false;
 
-		public:			
-			//コンストラクタ
-			StraightRunning();
-			
-			/**
-			 * @brief 直進する．
-		 	 * @param speed (-100~100)．	正の値で前進，負の値で後退
-			 */
-			void run(int speed);
-	};
+        public:
+            //コンストラクタ
+            StraightRunning();
+
+            /**
+             * @brief 直進する．
+             * @param speed (-100~100)．	正の値で前進，負の値で後退
+             */
+            void run(int speed);
+
+            /**
+             * @brief 加速の情報を初期化する
+             * @detail 全開に加速の途中でrun,changeSpeedをやめたときには初期化しなければならない
+             */
+            void initialize();
+
+
+            /**
+             * @brief 加速度を設定して直進する
+             *
+             * @param targetPwm 目標のPWM値(-100 ~ 100)
+             * @param acceleration 加速度の絶対値[pwm/s]
+             */
+            void run(int targetPwm, int acceleration);
+
+            /**
+             * @brief 加速度を設定して速度を変える
+             *
+             * @param targetPwm 目標のPWM値(-100 ~ 100)
+             * @param acceleration 加速度の絶対値[pwm/s]
+             *
+             * @return 目標のPWM値に達したらtrue
+             */
+            bool changeSpeed(int targetPwm, int acceleration);
+    };
 };
 #endif
