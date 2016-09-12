876873af6193539406ee8ef717ef2791fb13c20d
tsurumi <15nm721f@vc.ibaraki.ac.jp>
2016-09-12 18:54:11 +0900

    Revert "HIYOKOでカーブをライントレース無しで走行"
    This reverts commit 2f32343fe03e988ac787d2103992e3f782c7b34c.

--- strategy/RCourseStandard.cpp
+++ strategy/RCourseStandard.cpp
@@ -28,106 +28,45 @@ namespace strategy{
         break;
 
       case Status::STRAIGHT1_STANDBY:
-      	// 直線(1300)，カーブ()　直径(480)，直線()
-        distanceMeasurement_.setTargetDistance(1250);
+      	// 直線，カーブ，直線
+        distanceMeasurement_.setTargetDistance(3400);
         distanceMeasurement_.startMeasurement();
         Status_ = Status::STRAIGHT1;
         break;
       
       case Status::STRAIGHT1:
         if(!distanceMeasurement_.getResult()){
-          linetrace_->run(40,drive::LineTraceEdge::LEFT,0.6);
+          linetrace_->run(80,drive::LineTraceEdge::LEFT,0.6);
         }else{
           Status_ = Status::CURVE1_STANDBY;
         }
         break;
 
-      //第一カーブ部分
+      //直角に近いカーブ部分
       case Status::CURVE1_STANDBY:
-        distanceMeasurement_.setTargetDistance(744);
+        distanceMeasurement_.setTargetDistance(800);
         distanceMeasurement_.startMeasurement();
         Status_ = Status::CURVE1;
         break;
 
       case Status::CURVE1:
         if(!distanceMeasurement_.getResult()){
-          //成功(56,32)
-          curveRunning_.run(56, 32);
+          linetrace_->run(30,drive::LineTraceEdge::LEFT,0.6);
         }else{
           Status_ = Status::STRAIGHT2_STANDBY;
         }
         break;
 
       case Status::STRAIGHT2_STANDBY:
-        //直線2
-        distanceMeasurement_.setTargetDistance(1150);
+        //ゴールまで
+        distanceMeasurement_.setTargetDistance(4900);
         distanceMeasurement_.startMeasurement();
         Status_ = Status::STRAIGHT2;
         break;
 
       case Status::STRAIGHT2:
         if(!distanceMeasurement_.getResult()){
-          linetrace_->run(40,drive::LineTraceEdge::LEFT,0.6);
-        }else{
-          Status_ = Status::CURVE2_STANDBY;
-        }
-        break;
-
-      //直角に近いカーブ部分
-      case Status::CURVE2_STANDBY:
-        distanceMeasurement_.setTargetDistance(880);
-        distanceMeasurement_.startMeasurement();
-        Status_ = Status::CURVE2;
-        break;
-
-      case Status::CURVE2:
-        if(!distanceMeasurement_.getResult()){
-          curveRunning_.run(34, 58);
-        }else{
-          Status_ = Status::STRAIGHT3_STANDBY;
-        }
-        break;
-
-      case Status::STRAIGHT3_STANDBY:
-        //ゴールまで
-        distanceMeasurement_.setTargetDistance(1500);
-        distanceMeasurement_.startMeasurement();
-        Status_ = Status::STRAIGHT3;
-        break;
-
-      case Status::STRAIGHT3:
-        if(!distanceMeasurement_.getResult()){
-          linetrace_->run(40,drive::LineTraceEdge::LEFT,0.6);
-        }else{
-          Status_ = Status::CURVE3_STANDBY;
-        }
-        break;
-
-      //最後の直角カーブ部分
-      case Status::CURVE3_STANDBY:
-        distanceMeasurement_.setTargetDistance(400);
-        distanceMeasurement_.startMeasurement();
-        Status_ = Status::CURVE3;
-        break;
-
-      case Status::CURVE3:
-        if(!distanceMeasurement_.getResult()){
-          linetrace_->run(30,drive::LineTraceEdge::LEFT,0.6);
-        }else{
-          Status_ = Status::STRAIGHT4_STANDBY;
-        }
-        break;
-
-      case Status::STRAIGHT4_STANDBY:
-        //ゴールまで
-        distanceMeasurement_.setTargetDistance(3000);
-        distanceMeasurement_.startMeasurement();
-        Status_ = Status::STRAIGHT4;
-        break;
-
-      case Status::STRAIGHT4:
-        if(!distanceMeasurement_.getResult()){
-          linetrace_->run(40,drive::LineTraceEdge::LEFT,0.6);
+          linetrace_->run(80,drive::LineTraceEdge::LEFT,0.6);
         }else{
           Status_ = Status::DONE;
         }
--- strategy/RCourseStandard.h
+++ strategy/RCourseStandard.h
@@ -6,7 +6,6 @@
 #include "measurement/DistanceMeasurement.h"
 #include "device/Motors.h"
 #include "measurement/BodyAngleMeasurement.h"
-#include "drive/CurveRunning.h"
 
 namespace strategy{
 	class RCourseStandard : public IStrategy{
@@ -21,14 +20,6 @@ namespace strategy{
 				CURVE1,
 				STRAIGHT2_STANDBY,
 				STRAIGHT2,
-				CURVE2_STANDBY,
-				CURVE2,
-				STRAIGHT3_STANDBY,
-				STRAIGHT3,
-				CURVE3_STANDBY,
-				CURVE3,
-				STRAIGHT4_STANDBY,
-				STRAIGHT4,
 				DONE
 				};
 			Status Status_;
@@ -37,7 +28,6 @@ namespace strategy{
 			measurement::DistanceMeasurement distanceMeasurement_;
 			device::Motors* motor_;
 			measurement::BodyAngleMeasurement bodyAngleMeasurement_;
-			drive::CurveRunning curveRunning_;
 
 		public:
 			//コンストラクタ
