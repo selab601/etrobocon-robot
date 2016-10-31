788ad2ae88d7b99f6286f096963124410a00e93e
motoki nagaoka <16nm719r@vc.ibaraki.ac.jp>
2016-10-31 11:25:24 +0900

    インデントをスペースに

--- strategy/RCourseStandard.h
+++ strategy/RCourseStandard.h
@@ -9,42 +9,42 @@
 #include "drive/CurveRunning.h"
 
 namespace strategy{
-	class RCourseStandard : public IStrategy{
-		private:
-			enum class Status
-			{
-				DETECTION_STANDBY,
-				DETECTION_CURVE,
-				STRAIGHT1_STANDBY,
-				STRAIGHT1,
-				CURVE1_STANDBY,
-				CURVE1,
-				STRAIGHT2_STANDBY,
-				STRAIGHT2,
-				CURVE2_STANDBY,
-				CURVE2,
-				STRAIGHT3_STANDBY,
-				STRAIGHT3,
-				CURVE3_STANDBY,
-				CURVE3,
-				STRAIGHT4_STANDBY,
-				STRAIGHT4,
-				DONE
-				};
-			Status Status_;
+    class RCourseStandard : public IStrategy{
+        private:
+            enum class Status
+            {
+                DETECTION_STANDBY,
+                DETECTION_CURVE,
+                STRAIGHT1_STANDBY,
+                STRAIGHT1,
+                CURVE1_STANDBY,
+                CURVE1,
+                STRAIGHT2_STANDBY,
+                STRAIGHT2,
+                CURVE2_STANDBY,
+                CURVE2,
+                STRAIGHT3_STANDBY,
+                STRAIGHT3,
+                CURVE3_STANDBY,
+                CURVE3,
+                STRAIGHT4_STANDBY,
+                STRAIGHT4,
+                DONE
+            };
+            Status Status_;
 
-			drive::LineTrace* linetrace_;
-			measurement::DistanceMeasurement distanceMeasurement_;
-			device::Motors* motor_;
-			measurement::BodyAngleMeasurement bodyAngleMeasurement_;
-			drive::CurveRunning curveRunning_;
+            drive::LineTrace* linetrace_;
+            measurement::DistanceMeasurement distanceMeasurement_;
+            device::Motors* motor_;
+            measurement::BodyAngleMeasurement bodyAngleMeasurement_;
+            drive::CurveRunning curveRunning_;
 
-		public:
-			//コンストラクタ
-			RCourseStandard();
+        public:
+            //コンストラクタ
+            RCourseStandard();
 
-			bool capture();
-	};
+            bool capture();
+    };
 }
 
 #endif
