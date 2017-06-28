ac1f1bb4fa456905e609bd819651f09413c9d005
p <p@h181.n86.ibaraki.ac.jp>
2017-06-28 16:18:08 +0900

    インデントの修正

--- measurement/Coordinates.h
+++ measurement/Coordinates.h
@@ -9,35 +9,35 @@
 
 namespace measurement
 {
-	class Coordinates {
-	public:
-		Coordinates();
-		Coordinates(long,long);
-		void set_x(long);
-		void set_y(long);
-		void set_xy(long,long);
-		void set_x(double);
-		void set_y(double);
-		void set_xy(double,double);
-		long get_x();
-		long get_y();
-		/**
-		 * @brief 与えられた引数を,x,yのそれぞれに加算する
-		 */
-		void add_xy(long,long);
-		void add_xy(double,double);
-		/**
-		 * @brief x,yを0で初期化する
-		 */
-		void init();
-	private:
-		//座標の整数部分
-		long x_;
-		long y_;
-		//座標の小数部分
-		double dx_;
-		double dy_;
-	};
+    class Coordinates {
+    public:
+        Coordinates();
+        Coordinates(long,long);
+        void set_x(long);
+        void set_y(long);
+        void set_xy(long,long);
+        void set_x(double);
+        void set_y(double);
+        void set_xy(double,double);
+        long get_x();
+        long get_y();
+        /**
+         * @brief 与えられた引数を,x,yのそれぞれに加算する
+         */
+        void add_xy(long,long);
+        void add_xy(double,double);
+        /**
+         * @brief x,yを0で初期化する
+         */
+        void init();
+    private:
+        //座標の整数部分
+        long x_;
+        long y_;
+        //座標の小数部分
+        double dx_;
+        double dy_;
+    };
 }
 
 #endif /* COORDINATES_H_ */
