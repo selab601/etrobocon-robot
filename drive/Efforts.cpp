4b82bfbac7717fc239582af91c8ef7c4c247ca22
Usui Kakeru <kakeru3.141592@gmail.com>
2017-07-04 00:54:22 +0900

    見やすく整理

--- drive/Efforts.cpp
+++ drive/Efforts.cpp
@@ -5,25 +5,26 @@ using namespace detection;
 
 namespace drive{
     Efforts::Efforts(){
-        lineTrace_ = LineTrace::getInstance();
-        pivotTurn_ = new PivotTurn();
-        curveRunning_ = new CurveRunning();
-        blockColorGetter_ = new BlockColorGetter();
-        pushingOutRunning_ = new PushingOutRunning();
-        forcingOutRunning_ = new ForcingOutRunning();
-        lineDetection_ = new LineDetection();
-        colorDetection_ = new ColorDetection();
+        lineTrace_            = LineTrace::getInstance();
+        pivotTurn_            = new PivotTurn();
+        curveRunning_         = new CurveRunning();
+        blockColorGetter_     = new BlockColorGetter();
+        pushingOutRunning_    = new PushingOutRunning();
+        forcingOutRunning_    = new ForcingOutRunning();
+        lineDetection_        = new LineDetection();
+        colorDetection_       = new ColorDetection();
         rightAngledDetection_ = new RightAngledDetection();
 
-        procedureNumber_ = 0;
-        isSuccess_ = false;
         result_  = new colorset_t;
-        positionNumber_ = 0;
-        startEdge_ = LineTraceEdge::RIGHT;
-        endEdge_ = LineTraceEdge::RIGHT;
-        isRightCurve_ = false;
-        isRightForcingOut_ = false;
-        turnAngle_ = 0;
+
+        procedureNumber_    = 0;
+        isSuccess_          = false;
+        positionNumber_     = 0;
+        startEdge_          = LineTraceEdge::RIGHT;
+        endEdge_            = LineTraceEdge::RIGHT;
+        isRightCurve_       = false;
+        isRightForcingOut_  = false;
+        turnAngle_          = 0;
     }
 
     bool Efforts::run(int positionNumber){
@@ -49,35 +50,35 @@ namespace drive{
 
             switch(positionNumber_){
             case 1:
-                isRightForcingOut_ = true;
-                isRightCurve_ = true;
-                turnAngle_ = 200;
-                startEdge_ = LineTraceEdge::RIGHT;
-                endEdge_ = LineTraceEdge::LEFT;
+                isRightForcingOut_  = true;
+                isRightCurve_       = true;
+                turnAngle_          = 200;
+                startEdge_          = LineTraceEdge::RIGHT;
+                endEdge_            = LineTraceEdge::LEFT;
                 return true;
 
             case 2:
-                isRightForcingOut_ = false;
-                isRightCurve_ = false;
-                turnAngle_ = -200;
-                startEdge_ = LineTraceEdge::LEFT;
-                endEdge_ = LineTraceEdge::RIGHT;
+                isRightForcingOut_  = false;
+                isRightCurve_       = false;
+                turnAngle_          = -200;
+                startEdge_          = LineTraceEdge::LEFT;
+                endEdge_            = LineTraceEdge::RIGHT;
                 return true;
 
             case 3:
-                isRightForcingOut_ = false;
-                isRightCurve_ = true;
-                turnAngle_ = -150;
-                startEdge_ = LineTraceEdge::RIGHT;
-                endEdge_ = LineTraceEdge::LEFT;
+                isRightForcingOut_  = false;
+                isRightCurve_       = true;
+                turnAngle_          = -150;
+                startEdge_          = LineTraceEdge::RIGHT;
+                endEdge_            = LineTraceEdge::LEFT;
                 return true;
 
             case 4:
-                isRightForcingOut_ = true;
-                isRightCurve_ = false;
-                turnAngle_ = 150;
-                startEdge_ = LineTraceEdge::LEFT;
-                endEdge_ = LineTraceEdge::RIGHT;
+                isRightForcingOut_  = true;
+                isRightCurve_       = false;
+                turnAngle_          = 150;
+                startEdge_          = LineTraceEdge::LEFT;
+                endEdge_            = LineTraceEdge::RIGHT;
                 return true;
 
             default: return false;
