c1d2006bbaded1cbb1e648d09a941c6911db73d1
motoki nagaoka <motoki.23ar@gmail.com>
2017-07-09 21:31:20 +0900

    device/ColorSensor にrgb, hsv取得メソッド追加 (ToDo: ColorDetectionからRgb2HSV()の削除)

--- device/ColorSensor.cpp
+++ device/ColorSensor.cpp
@@ -1,4 +1,5 @@
 #include "ColorSensor.h"
+using namespace std;
 
 namespace device
 {
@@ -18,7 +19,10 @@ namespace device
         ev3api::ColorSensor(PORT_3),
         whiteCalibratedValue_(0),
         blackCalibratedValue_(0)
-    {}
+    {
+        rgb_.r = rgb.g = rgb.b = 0;
+        hsv_.h = hsv_.s = hsv_.v = 0;
+    }
 
     void ColorSensor::setCalibrateValue(int8_t whiteValue, int8_t blackValue){
         this->whiteCalibratedValue_ = whiteValue;
@@ -50,5 +54,64 @@ namespace device
         default:
             return "UNDEFINED";
         }
+    }
+
+    void ColorSensor::updateColor(){
+        ev3api::ColorSensor::getRawColor(rgb_);
+        hsv_ = rgb2Hsv(rgb_);
+    }
+
+    int ColorSensor::getR(){return rgb_.r;}
+    int ColorSensor::getG(){return rgb_.g;}
+    int ColorSensor::getB(){return rgb_.b;}
+
+    int ColorSensor::getH(){return hsv_.h;}
+    int ColorSensor::getS(){return hsv_.s;}
+    int ColorSensor::getV(){return hsv_.v;}
+
+    hsv_raw_t ColorSensor::rgb2Hsv(rgb_raw_t rgbValue) {
+        vector<float> rgb {
+            (float)rgbValue.r,
+            (float)rgbValue.g,
+            (float)rgbValue.b
+        };
+
+        // 255より大きいRGB値は255に合わせる
+        for_each(rgb.begin(), rgb.end(), [](float &v){ if (v > 255) { v = 255; } });
+
+        vector<float>::iterator minPtr = min_element(rgb.begin(), rgb.end());
+        vector<float>::iterator maxPtr = max_element(rgb.begin(), rgb.end());
+        // 接頭辞 i は，index を意味する．
+        // iMaxValue は maxValue の index．iMinValue も同様
+        int iMaxValue = (int)distance(rgb.begin(), maxPtr);
+        int iMinValue = (int)distance(rgb.begin(), minPtr);
+        float maxValue = *maxPtr;
+        float minValue = *minPtr;
+
+        int h;
+        if (iMaxValue == iMinValue) {
+            h = 0;
+        } else if (iMaxValue == 0 /* R値が最も高い */) {
+            h = (int)(60*(rgb.at(1)-rgb.at(2))/(maxValue-minValue)+360)%360;
+        } else if (iMaxValue == 1 /* G値が最も高い */) {
+            h = (int)(60*(rgb.at(2)-rgb.at(0))/(maxValue-minValue))+120;
+        } else if (iMaxValue == 2 /* B値が最も高い */) {
+            h = (int)(60*(rgb.at(0)-rgb.at(1))/(maxValue-minValue))+240;
+        } else {
+            // WARNING: ここにはこないはず
+            h = 0;
+        }
+
+        int s;
+        if (maxValue == 0) {
+            s = 0;
+        } else {
+            s = (255*((maxValue - minValue)/maxValue));
+        }
+
+        int v = maxValue;
+
+        hsv_raw_t hsv = {h,s,v};
+        return hsv;
     }
 }
