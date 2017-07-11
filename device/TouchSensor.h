496977b63f2469038f596747c7bf2e3a365d8c75
motoki nagaoka <motoki.23ar@gmail.com>
2017-07-11 23:35:20 +0900

    TouchSensor::isClicked()を作成 不本意にタッチセンサが反応しないように0.5秒以上間隔を開けて反応する

--- device/TouchSensor.h
+++ device/TouchSensor.h
@@ -2,6 +2,7 @@
 #define TOUCHSENSOR_H_
 
 #include "/home/vagrant/hrp2/sdk/common/library/libcpp-ev3/include/TouchSensor.h"
+#include "measurement/timeMeasurement.h"
 
 namespace device
 {
@@ -10,11 +11,22 @@ namespace device
     private:
         static TouchSensor* instance_;
 
+        bool hasPressed_ = false;
+        measurement::TimeMeasurement sinceLastClicked_;
+
     private:
         TouchSensor();
 
     public:
         static TouchSensor* getInstance();
+
+        /**
+         * @brief クリックされた(押して離した)時trueを返す
+         * @details 前回クリックされてから0.5秒経過後でないとクリック判定されない
+         *
+         * @return クリックされたか
+         */
+        bool isClicked();
     };
 }
 
