0537e159955b0bdda5b27f05d5cebc8288399224
kunokuno <hnkw_kuno@yahoo.co.jp>
2017-07-13 16:41:41 +0900

    add encodeBlack

--- contest/BlockCode.cpp
+++ contest/BlockCode.cpp
@@ -19,10 +19,14 @@ namespace contest_pkg {
     }
 
     void BlockCode::setCode(int initPositionCode){
-        black_  = initPositionCode / 1331 + 1;
-        red_    = (initPositionCode - 1331*(black_-1) ) / 121 + 1;
-        yellow_ = (initPositionCode - 1331*(black_-1) - 121*(red_-1)) / 11 + 1;
-        blue_   = initPositionCode - 1331*(black_-1) - 121*(red_-1) - 11*(yellow_-1) + 1;
+        black_  =  initPositionCode / 1331 + 1;
+        red_    =( initPositionCode - 1331*(black_-1) ) / 121 + 1;
+        yellow_ =( initPositionCode - 1331*(black_-1) - 121*(red_-1) ) / 11 + 1;
+        blue_   =  initPositionCode - 1331*(black_-1) - 121*(red_-1) - 11*(yellow_-1) + 1;
+
+        red_ = encodeRed(red_-1);
+        blue_ = encodeBlue(blue_-1);
+        yellow_ = encodeYellow(yellow_-1);
     }
 
     int BlockCode::getIdRed(){
@@ -31,14 +35,25 @@ namespace contest_pkg {
     int BlockCode::getIdBlue(){
         return blue_;
     }
-    int BlockCode::getIdGreen(){
-        return green_;
-    }
     int BlockCode::getIdYellow(){
         return yellow_;
     }
+    int BlockCode::getIdGreen(){
+        return green_;
+    }
     int BlockCode::getIdBlack(){
         return black_;
+    }
+
+
+    int BlockCode::encodeRed(int redCode){
+        return redToBlalck_[redCode];
+    }
+    int BlockCode::encodeBlue(int blueCode){
+        return blueToBlack_[blueCode];
+    }
+    int BlockCode::encodeYellow(int yellowCode){
+        return yellowToBlack_[yellowCode];
     }
 
 }
--- contest/BlockCode.h
+++ contest/BlockCode.h
@@ -1,23 +1,30 @@
-#ifndef BLOCKCODE_
-#define BLOCKCODE_
+    #ifndef BLOCKCODE_
+    #define BLOCKCODE_
 
-#include "math.h"
-#include "stdio.h"
+    #include "math.h"
+    #include "stdio.h"
+    #include <array> 
 
-namespace contest_pkg {
-    class BlockCode{
-    private:
+    namespace contest_pkg {
+        class BlockCode{
+        private:
 
-        static BlockCode* instance_;
-        BlockCode();
+            static BlockCode* instance_;
+            BlockCode();
 
-        int red_;
-        int blue_;
-        int yellow_;
-        int green_;
-        int black_;
+            int red_;
+            int blue_;
+            int yellow_;
+            int green_;
+            int black_;
+            
+            int redToBlalck_[11]   = {2,3,4,5,6,9,10,11,12,13,15};
+            int blueToBlack_[11]   = {1,3,5,6,7,8,10,11,13,14,15};
+            int yellowToBlack_[11] = {1,2,4,6,7,8,9,10,11,12,14};
         
-        
+            int encodeRed(int redCode);
+            int encodeBlue(int blueCode);
+            int encodeYellow(int yellowCode);
 
     public:
         static BlockCode* getInstance();
