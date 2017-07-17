84cf8858c527d11b512571d961a43e6977f0c9fd
Usui Kakeru <kakeru3.141592@gmail.com>
2017-07-17 17:13:21 +0900

    去年のコードを削除

--- drive/Catching.h
+++ drive/Catching.h
@@ -11,7 +11,6 @@
 #include "../device/ColorSensor.h"
 #include <stdlib.h>
 
-#define TO_LINE_LENGTH 165  // 6センチになる！
 namespace drive
 {
     class Catching{
@@ -27,76 +26,14 @@ namespace drive
                 FINISHED,
             };
 
-            // 方向を変えてから取得するときの状態
-            enum class ChangeDirectionState{
-                INIT,
-                AVOIDANCE,
-                TURN,
-                TURN_TO_LINE,
-                CATCHING,
-                FINISHED,
-            };
-
-            enum class TurnState{
-                INIT,
-                TURN,
-                FINISHED,
-            };
             State state_ = State::INIT;
-            TurnState turnState_ = TurnState::INIT;
 
-            Destination* destination_;
 
         public:
             Catching();
 
-            enum class TurnDirection{
-                RIGHT,
-                LEFT,
-                STRAIGHT,
-                BACK,
-            };
-
-            /**
-             * @brief ブロックを取得する
-             *
-             * @param x 目的地のx座標
-             * @param y 目的地のy座標
-             *
-             * @return 終了したらtrue
-             */
-            bool catchBlock(int x, int y);
-
-
-            /**
-             * @brief ブロックを取得する
-             *
-             * @param destination 目的地の座標
-             *
-             * @return 終了したらtrue
-             */
-            bool catchBlock(BlockAreaCoordinate destination);
-
-            /**
-             * @brief ブロックを取得する
-             *
-             * @param direction 取得してから進む方向
-             *
-             * @return 終了したらtrue
-             */
-            bool catchBlock(TurnDirection direction );
-
-            bool putBlock();
-
         private:
-            bool catchBackBlock();
-            bool turn(int degree);
-            bool turn(TurnDirection direction);
-            bool straight(int length);
-            DirectionKind getAdvancableDirection();
 
-            bool daizaDetected();
-            void stop();
     };
 }
 #endif
