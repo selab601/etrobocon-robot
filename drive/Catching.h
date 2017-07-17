d496df87879e240fa64a1231cdbcc6a9543a0f04
Usui Kakeru <kakeru3.141592@gmail.com>
2017-07-17 17:16:19 +0900

    いらないもの削除

--- drive/Catching.h
+++ drive/Catching.h
@@ -1,15 +1,10 @@
 #ifndef CATCHING_H
 #define CATCHING_H
 
-#include "./PivotTurn.h"
-#include "./Destination.h"
-#include "./Avoidance.h"
 #include "../measurement/BodyAngleMeasurement.h"
 #include "./CurveRunning.h"
 #include "./LineTrace.h"
 #include "../detection/ColorDetection.h"
-#include "../device/ColorSensor.h"
-#include <stdlib.h>
 
 namespace drive
 {
