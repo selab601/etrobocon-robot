4100cd2b6fa0c80bb94ff832137317f9ecfb065e
motoki nagaoka <motoki.23ar@gmail.com>
2017-07-21 06:26:20 +0900

    ラインの中心からラインの中心までをまっすぐ移動する
    45cm->45cmでのときのみ作った
    
    現在ラインと目的地ラインの長さと、角度を指定して移動できるものを作成
    (TODO: エッジの更新・ まっすぐ行くときの回避の作成)
    
    hasBlock()追加

--- drive/Avoidance.cpp
+++ drive/Avoidance.cpp
@@ -9,6 +9,7 @@ namespace drive{
 
         bodyAngle_ = new measurement::BodyAngleMeasurement();
         state_ = State::START;
+        runToState_ = RunToState::INIT;
     }
     bool Avoidance::startAvoidance(DirectionKind kind){
         switch (kind) {
@@ -173,5 +174,54 @@ namespace drive{
             return false;
         }
         return false;
+    }
+
+    // TODO: 直線に近いときにきちんと台座回避する
+    // TODO: ライントレースのエッジをセットする
+    // TODO: ブロックを離さないようにする
+    bool Avoidance::runTo(int currentMm, int dstMMm, int degree){
+        static int x = 300;
+        static int y = 300;
+        static int turnStartDegree = 0;
+        switch (runToState_){
+            case RunToState::INIT:
+                {
+                bodyAngle_->setBaseAngle();
+                double rad = degree * M_PI /180;
+                x = dstMMm * cos(rad) / 2;
+                y = dstMMm * sin(rad) / 2;
+                x += currentMm / 2;
+                polar_.centerPivot(!hasBlock_);  // ブロックを離さないように
+                polar_.setMaxPwm(20);
+                runToState_ = RunToState::POLAR;
+                ev3_speaker_play_tone(200, 100);
+                }
+                break;
+
+            case RunToState::POLAR:
+                if (polar_.runToXY(x, y)){
+                    runToState_ = RunToState::TURN;
+                    turnStartDegree = bodyAngle_->getResult();
+                    ev3_speaker_play_tone(550, 100);
+                }
+                break;
+
+            case RunToState::TURN:
+                polar_.centerPivot(true);
+                if ( polar_.bodyTurn(degree - turnStartDegree, 20) ){
+                    runToState_ = RunToState::FINISHED;
+                    ev3_speaker_play_tone(800, 100);
+                }
+                break;
+
+            case RunToState::FINISHED:
+                    runToState_ = RunToState::INIT;
+                return true;
+        }
+        return false;
+    }
+
+    void Avoidance::hasBlock(bool hasBlock){
+        hasBlock_ = hasBlock;
     }
 }
--- drive/Avoidance.h
+++ drive/Avoidance.h
@@ -5,6 +5,7 @@
 #include "measurement/BodyAngleMeasurement.h"
 #include "CurveRunning.h"
 #include "StraightRunning.h"
+#include "PolarRunning.h"
 #include "ev3api.h"
 
 namespace drive{
@@ -16,6 +17,13 @@ namespace drive{
         LEFT,
         STRAIGHT_RIGHT,
         STRAIGHT_LEFT
+    };
+
+    enum class RunToState{
+        INIT,
+        POLAR,
+        TURN,
+        FINISHED
     };
 
     /**
@@ -39,6 +47,13 @@ namespace drive{
         detection::LineDetection* lineDetection_;
         measurement::BodyAngleMeasurement* bodyAngle_;
         State state_;
+
+        // runTo
+        RunToState runToState_ = RunToState::INIT;
+        int startDegree_;
+        int polarMm_;
+        int polarDegree_;
+        PolarRunning polar_;
 
     public:
         //コンストラクタ
@@ -80,6 +95,29 @@ namespace drive{
          * @return 回避走行が終了したらtrueを返す
          **/
         bool straightLeft();
+
+
+        /**
+         * @brief ブロック置き場を回避して他のラインに移動する
+         * @details PolarRunningを利用して、他のラインに移動する
+         *      ラインの真ん中に居て、ラインと平行になっている必要がある
+         * @param currentMm 現在のラインの長さ[mm]
+         * @param dstMMm 目的地ラインの長さ[mm]
+         * @param degree 現在のラインからみた、目的地ラインの曲がった角度[degree]
+         *
+         * @return 終了したらtrue
+         */
+        bool runTo(int currentMm, int dstMMm, int degree);
+
+
+        /**
+         * @brief ブロックを持っているか知らせる
+         * @details ブロックを持っている場合は離さないように超信地旋回を避ける
+         *
+         * @param hasBlock ブロックを持っているときtrue
+         *
+         */
+        void hasBlock(bool hasBlock);
 
     };
 
